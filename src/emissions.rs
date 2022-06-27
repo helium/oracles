2cab6cab3f14a36dc5fada68fa31220f85b84a22
jeffgrunewald <jeff.grunewald@gmail.com>
2022-06-27 08:30:20 -0400

    handling missing models test

--- src/emissions.rs
+++ src/emissions.rs
@@ -30,14 +30,16 @@ pub fn get_emissions_per_model(models: HashMap<Model, u64>, datetime: DateTime<U
     let ffser_shares = *ffsercomms as f64 * FFSERWT;
     let total_weights = ff436_shares + ff430_shares + ffser_shares;
     let base_reward = total_rewards as f64 / total_weights;
+    let ff436_rewards = if ff436s > &0 { f64::trunc((base_reward * FF436WT) * PRECISION) / PRECISION } else { 0.0 };
+    let ff430_rewards = if ff430s > &0 { f64::trunc((base_reward * FF430WT) * PRECISION) / PRECISION } else { 0.0 };
+    let ffsercomm_rewards = if ffsercomms > &0 { f64::trunc((base_reward * FFSERWT) * PRECISION) / PRECISION } else { 0.0 };
+    // let ff436_rewards = if ff436s > &0 { (base_reward * FF436WT).round() as u64 } else { 0 };
+    // let ff430_rewards = if ff430s > &0 { (base_reward * FF430WT).round() as u64 } else { 0 };
+    // let ffsercomm_rewards = if ffsercomms > &0 { (base_reward * FFSERWT).round() as u64 } else { 0 };
     HashMap::from([
-        (Model::FF436, f64::trunc((base_reward * FF436WT) * PRECISION) / PRECISION),
-        (Model::FF430, f64::trunc((base_reward * FF430WT) * PRECISION) / PRECISION),
-        (Model::FFSercomm, f64::trunc((base_reward * FFSERWT) * PRECISION) / PRECISION),
-        // In case we need to fully truncate the rewards and not denominate tokens in decimals
-        // (Model::FF436, (base * FF436WT).round() as f64),
-        // (Model::FF430, (base * FF430WT).round() as f64),
-        // (Model::FFSercomm, (base * FFSERWT).round() as f64),
+        (Model::FF436, ff436_rewards),
+        (Model::FF430, ff430_rewards),
+        (Model::FFSercomm, ffsercomm_rewards),
     ])
 }
 
@@ -80,10 +82,11 @@ mod test {
         assert_eq!(expected, get_emissions_per_model(input, date))
     }
 
-    // fn no_436s_reward() {
-    //     let expected = HashMap::from([(Model::FFSercomm, ), (Model::FF430, ), (Model::FF436, )]);
-    //     let date = Utc.ymd(2022, 7, 17).and_hms(0, 0, 0);
-    //     let input = HashMap::from([(Model::FFSercomm, 20), (Model::FF430, 15), (Model::FF436, 0)]);
-    //     assert_eq!(expected, get_emissions_per_model(input, date))
-    // }
+    #[test]
+    fn no_436s_reward() {
+        let expected = HashMap::from([(Model::FFSercomm, 44817.9), (Model::FF430, 67226.85), (Model::FF436, 0.0)]);
+        let date = Utc.ymd(2022, 7, 17).and_hms(0, 0, 0);
+        let input = HashMap::from([(Model::FFSercomm, 20), (Model::FF430, 15), (Model::FF436, 0)]);
+        assert_eq!(expected, get_emissions_per_model(input, date))
+    }
 }
