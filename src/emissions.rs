9ebec3504ebb4daa46d72fc0e1bc0db567a1d7f3
Rahul Garg <rg@vihu.dev>
2022-07-13 12:36:28 -0600

    Fix hotspot models

--- src/emissions.rs
+++ src/emissions.rs
@@ -21,14 +21,14 @@ pub fn get_emissions_per_model(
     let total_rewards = get_scheduled_tokens(datetime)
         .expect("Failed to supply valid date on the emission schedule");
 
-    let nova436s = models.get(&Model::Nova436H).unwrap_or_else(|| &0);
-    let nova430s = models.get(&Model::Nova430H).unwrap_or_else(|| &0);
-    let sercommos = models.get(&Model::SercommOutdoor).unwrap_or_else(|| &0);
-    let sercommis = models.get(&Model::SercommIndoor).unwrap_or_else(|| &0);
-    let neut430s = models.get(&Model::Neutrino430).unwrap_or_else(|| &0);
+    let nova436h = models.get(&Model::Nova436H).unwrap_or(&0);
+    let nova430i = models.get(&Model::Nova430I).unwrap_or(&0);
+    let sercommos = models.get(&Model::SercommOutdoor).unwrap_or(&0);
+    let sercommis = models.get(&Model::SercommIndoor).unwrap_or(&0);
+    let neut430s = models.get(&Model::Neutrino430).unwrap_or(&0);
 
-    let nova436_shares = Hotspot::NOVA436H.reward_shares(*nova436s);
-    let nova430_shares = Hotspot::NOVA430H.reward_shares(*nova430s);
+    let nova436_shares = Hotspot::NOVA436H.reward_shares(*nova436h);
+    let nova430_shares = Hotspot::NOVA430I.reward_shares(*nova430i);
     let sercommo_shares = Hotspot::SERCOMMOUTDOOR.reward_shares(*sercommos);
     let sercommi_shares = Hotspot::SERCOMMINDOOR.reward_shares(*sercommis);
     let neut430_shares = Hotspot::NEUTRINO430.reward_shares(*neut430s);
@@ -36,13 +36,13 @@ pub fn get_emissions_per_model(
     let total_weights =
         nova436_shares + nova430_shares + sercommo_shares + sercommi_shares + neut430_shares;
     let base_reward = total_rewards as f64 / total_weights;
-    let nova436_rewards = if nova436s > &0 {
+    let nova436_rewards = if nova436h > &0 {
         Hotspot::NOVA436H.rewards(base_reward, PRECISION)
     } else {
         0.0
     };
-    let nova430_rewards = if nova430s > &0 {
-        Hotspot::NOVA430H.rewards(base_reward, PRECISION)
+    let nova430_rewards = if nova430i > &0 {
+        Hotspot::NOVA430I.rewards(base_reward, PRECISION)
     } else {
         0.0
     };
@@ -64,7 +64,7 @@ pub fn get_emissions_per_model(
 
     HashMap::from([
         (Model::Nova436H, nova436_rewards),
-        (Model::Nova430H, nova430_rewards),
+        (Model::Nova430I, nova430_rewards),
         (Model::SercommOutdoor, sercommo_rewards),
         (Model::SercommIndoor, sercommi_rewards),
         (Model::Neutrino430, neut430_rewards),
@@ -89,7 +89,7 @@ mod test {
     fn genesis_reward() {
         let expected = HashMap::from([
             (Model::SercommOutdoor, 30557.66),
-            (Model::Nova430H, 30557.66),
+            (Model::Nova430I, 30557.66),
             (Model::Nova436H, 40743.55),
             (Model::SercommIndoor, 20371.77),
             (Model::Neutrino430, 20371.77),
@@ -97,7 +97,7 @@ mod test {
         let date = Utc.ymd(2022, 7, 17).and_hms(0, 0, 0);
         let input = HashMap::from([
             (Model::SercommOutdoor, 20),
-            (Model::Nova430H, 15),
+            (Model::Nova430I, 15),
             (Model::Nova436H, 10),
             (Model::SercommIndoor, 13),
             (Model::Neutrino430, 8),
@@ -109,7 +109,7 @@ mod test {
     fn post_genesis_reward() {
         let expected = HashMap::from([
             (Model::SercommOutdoor, 61115.34),
-            (Model::Nova430H, 61115.34),
+            (Model::Nova430I, 61115.34),
             (Model::Nova436H, 81487.12),
             (Model::SercommIndoor, 40743.56),
             (Model::Neutrino430, 40743.56),
@@ -117,7 +117,7 @@ mod test {
         let date = Utc.ymd(2023, 1, 1).and_hms(0, 0, 0);
         let input = HashMap::from([
             (Model::SercommOutdoor, 20),
-            (Model::Nova430H, 15),
+            (Model::Nova430I, 15),
             (Model::Nova436H, 10),
             (Model::SercommIndoor, 13),
             (Model::Neutrino430, 8),
@@ -129,7 +129,7 @@ mod test {
     fn no_reporting_model_reward() {
         let expected = HashMap::from([
             (Model::SercommOutdoor, 38872.67),
-            (Model::Nova430H, 38872.67),
+            (Model::Nova430I, 38872.67),
             (Model::Nova436H, 0.0),
             (Model::SercommIndoor, 25915.11),
             (Model::Neutrino430, 25915.11),
@@ -137,7 +137,7 @@ mod test {
         let date = Utc.ymd(2022, 7, 17).and_hms(0, 0, 0);
         let input = HashMap::from([
             (Model::SercommOutdoor, 20),
-            (Model::Nova430H, 15),
+            (Model::Nova430I, 15),
             (Model::SercommIndoor, 13),
             (Model::Neutrino430, 8),
         ]);
--- src/hotspot.rs
+++ src/hotspot.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Eq, Hash, PartialEq)]
 pub enum Model {
     Nova436H,
-    Nova430H,
+    Nova430I,
     Neutrino430,
     SercommIndoor,
     SercommOutdoor,
@@ -19,8 +19,8 @@ impl<'a> Hotspot<'a> {
         fcc_id: "2AG32MBS3100196N",
         reward_wt: 2.0,
     };
-    pub const NOVA430H: Hotspot<'a> = Hotspot {
-        model: Model::Nova430H,
+    pub const NOVA430I: Hotspot<'a> = Hotspot {
+        model: Model::Nova430I,
         fcc_id: "2AG32PBS3101S",
         reward_wt: 1.5,
     };
@@ -40,19 +40,11 @@ impl<'a> Hotspot<'a> {
         reward_wt: 1.5,
     };
 
-    pub const MODELS: [Hotspot<'a>; 5] = [
-        Hotspot::NOVA436H,
-        Hotspot::NOVA430H,
-        Hotspot::NEUTRINO430,
-        Hotspot::SERCOMMINDOOR,
-        Hotspot::SERCOMMOUTDOOR,
-    ];
-
     pub fn reward_shares(&self, units: u64) -> f64 {
-        &self.reward_wt * units as f64
+        self.reward_wt * units as f64
     }
 
     pub fn rewards(&self, base_rewards: f64, precision: f64) -> f64 {
-        f64::trunc((&self.reward_wt * base_rewards) * precision) / precision
+        f64::trunc((self.reward_wt * base_rewards) * precision) / precision
     }
 }
