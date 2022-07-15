871bc191862ace35ed5e836925a7aef4608315cf
Marc Nijdam <marc_nijdam@mac.com>
2022-07-15 07:31:52 -0400

    Use heartbeat as source of truth

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,13 +4,12 @@ pub mod heartbeat;
 pub mod server;
 pub mod speedtest;
 
-use crate::Error;
+use crate::{follower::FollowerService, Error};
 use axum::{
     async_trait,
     extract::{Extension, FromRequest, RequestParts},
     http::StatusCode,
 };
-use sqlx::postgres::PgPool;
 
 /// Utility function for mapping any error into an api error
 pub fn api_error<E>(err: E) -> (StatusCode, String)
@@ -21,23 +20,20 @@ where
     Error::from(err).into()
 }
 
-// A custom extractor that grabs a connection from the pool
-pub struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+pub struct Follower(FollowerService);
 
 #[async_trait]
-impl<B> FromRequest<B> for DatabaseConnection
+impl<B> FromRequest<B> for Follower
 where
     B: Send,
 {
     type Rejection = (StatusCode, String);
 
     async fn from_request(req: &mut RequestParts<B>) -> std::result::Result<Self, Self::Rejection> {
-        let Extension(pool) = Extension::<PgPool>::from_request(req)
+        let Extension(follower) = Extension::<FollowerService>::from_request(req)
             .await
             .map_err(api_error)?;
 
-        let conn = pool.acquire().await.map_err(api_error)?;
-
-        Ok(Self(conn))
+        Ok(Self(follower))
     }
 }
