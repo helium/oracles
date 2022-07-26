8e6a8a69fe65f5365f6f7581a059efa315a31ca6
Rahul Garg <rg@vihu.dev>
2022-07-26 16:02:21 -0600

    Write CSV to stdout

--- store/src/cli/dump.rs
+++ store/src/cli/dump.rs
@@ -1,6 +1,7 @@
 use crate::{heartbeat::CellHeartbeat, FileSource, Result};
 use csv::Writer;
 use helium_proto::{services::poc_mobile::CellHeartbeatReqV1, Message};
+use std::io;
 use std::path::PathBuf;
 
 /// Print information about a given store file.
@@ -8,15 +9,13 @@ use std::path::PathBuf;
 pub struct Cmd {
     /// Path for heartbeat/speedtest
     in_path: PathBuf,
-    /// Path for dumping CSV
-    out_path: PathBuf,
 }
 
 impl Cmd {
     pub async fn run(&self) -> Result {
         let mut file_source = FileSource::new(&self.in_path).await?;
 
-        let mut wtr = Writer::from_path(&self.out_path)?;
+        let mut wtr = Writer::from_writer(io::stdout());
         while let Some(msg) = file_source.read().await? {
             let dec_msg = CellHeartbeatReqV1::decode(msg)?;
             wtr.serialize(CellHeartbeat::try_from(dec_msg)?)?;
