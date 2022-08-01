30dbe7c05aabc1618e770e41d1c16aabfe9ed6a2
Rahul Garg <rg@vihu.dev>
2022-08-01 14:57:59 -0600

    Fix and add test for FileMultiSource

--- store/src/file_multi_source.rs
+++ store/src/file_multi_source.rs
@@ -23,18 +23,12 @@ impl<'a> FileMultiSource<'a> {
 #[async_trait]
 impl<'a> FileSourceRead<'a> for FileMultiSource<'a> {
     async fn read(&mut self) -> Result<Option<BytesMut>> {
-        if self.0.is_empty() {
-            return Ok(None);
-        }
-
-        let mut optional = self.0.pop();
-
-        while let Some(mut cur_src) = optional {
+        while let Some(cur_src) = self.0.last_mut() {
             match cur_src.read().await? {
                 Some(result) => return Ok(Some(result)),
                 None => {
-                    optional = self.0.pop();
-                    continue;
+                    // Current source exhausted, pop and move to next one
+                    self.0.pop();
                 }
             }
         }
@@ -44,28 +38,35 @@ impl<'a> FileSourceRead<'a> for FileMultiSource<'a> {
 
 #[cfg(test)]
 mod test {
-    use std::path::PathBuf;
-
     use super::*;
-    use crate::{heartbeat::CellHeartbeat, FileSourceRead};
-    use csv::Writer;
-    use helium_proto::{services::poc_mobile::CellHeartbeatReqV1, Message};
-    use std::io;
+    use std::path::PathBuf;
 
     #[tokio::test]
     async fn test_multi_read() {
+        async fn get_count(mut file_src: FileSource<'_>) -> Result<i32> {
+            let mut count = 0;
+            while let Some(_buf) = file_src.read().await.unwrap() {
+                count += 1;
+            }
+            Ok(count)
+        }
+
         let p1 = PathBuf::from(r"../test/cell_heartbeat.1658832527866.gz");
         let p2 = PathBuf::from(r"../test/cell_heartbeat.1658834120042.gz");
         let mut file_multi_src = FileMultiSource::new(&[p1.as_path(), p2.as_path()])
             .await
             .unwrap();
-        let mut wtr = Writer::from_writer(io::stdout());
-        while let Some(msg) = file_multi_src.read().await.unwrap() {
-            let dec_msg = CellHeartbeatReqV1::decode(msg).unwrap();
-            wtr.serialize(CellHeartbeat::try_from(dec_msg).unwrap())
-                .unwrap();
+
+        let mut tot_count = 0;
+        while let Some(_buf) = file_multi_src.read().await.unwrap() {
+            tot_count += 1;
         }
 
-        wtr.flush().unwrap();
+        let f1 = FileSource::new(p1.as_path()).await.unwrap();
+        let c1 = get_count(f1).await.unwrap();
+        let f2 = FileSource::new(p2.as_path()).await.unwrap();
+        let c2 = get_count(f2).await.unwrap();
+
+        assert_eq!(tot_count, c1 + c2);
     }
 }
