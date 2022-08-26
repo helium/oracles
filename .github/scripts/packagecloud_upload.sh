9bc368727d51c5314a46a8dbff6cc28053cc1595
Matthew Plant <matty@nova-labs.com>
2022-08-26 11:39:34 -0400

    Debian package uploading for reward and ingest servers to run on tag  (#27)
    cargo deb and upload built packages to packagecloud private repos on tag
    
    Co-authored-by: Matthew Plant <map@Matthews-MacBook-Pro-2.local>

---
+++ .github/scripts/packagecloud_upload.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+MOBILE_REWARDS_PKGNAME=$( find target/debian -type f -iname "poc5g-rewards_*.deb" )
+MOBILE_INGEST_PKGNAME=$( find target/debian -type f -iname "poc5g-ingest_*.deb" )
+
+curl -u "${PACKAGECLOUD_API_KEY}:" \
+     -F "package[distro_version_id]=210" \
+     -F "package[package_file]=@${MOBILE_REWARDS_PKGNAME}" \
+     https://packagecloud.io/api/v1/repos/helium/mobile_rewards/packages.json
+
+curl -u "${PACKAGECLOUD_API_KEY}:" \
+     -F "package[distro_version_id]=210" \
+     -F "package[package_file]=@${MOBILE_INGEST_PKGNAME}" \
+     https://packagecloud.io/api/v1/repos/helium/mobile_ingest/packages.json
