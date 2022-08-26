f0d3e2b6bcd504922b3c00c4ba68807bb04c2135
Rahul Garg <rg@vihu.dev>
2022-08-26 00:56:57 -0600

    Add support to dump cell_speedtest files

--- store/src/speedtest.rs
+++ store/src/speedtest.rs
@@ -1,9 +1,9 @@
-use crate::{Error, PublicKey, Result};
+use crate::{datetime_from_epoch, Error, PublicKey, Result};
 use chrono::{DateTime, Utc};
 use helium_proto::services::poc_mobile::SpeedtestReqV1;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct CellSpeedtest {
     #[serde(alias = "pubKey")]
     pub pubkey: PublicKey,
@@ -14,6 +14,20 @@ pub struct CellSpeedtest {
     #[serde(alias = "downloadSpeed")]
     pub download_speed: u64,
     pub latency: u32,
+}
+
+impl TryFrom<SpeedtestReqV1> for CellSpeedtest {
+    type Error = Error;
+    fn try_from(v: SpeedtestReqV1) -> Result<Self> {
+        Ok(Self {
+            pubkey: PublicKey::try_from(v.pub_key)?,
+            serial: v.serial,
+            timestamp: datetime_from_epoch(v.timestamp),
+            upload_speed: v.upload_speed,
+            download_speed: v.download_speed,
+            latency: v.latency,
+        })
+    }
 }
 
 impl TryFrom<CellSpeedtest> for SpeedtestReqV1 {
