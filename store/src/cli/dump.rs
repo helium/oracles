bf3f6e66909b16a62c8134246ea7d7b378c93628
Rahul Garg <rg@vihu.dev>
2022-08-26 00:56:57 -0600

    Add support to dump cell_speedtest files

--- store/src/cli/dump.rs
+++ store/src/cli/dump.rs
@@ -1,9 +1,13 @@
-use crate::{file_source, heartbeat::CellHeartbeat, Result};
+use crate::{file_source, heartbeat::CellHeartbeat, speedtest::CellSpeedtest, FileType, Result};
 use csv::Writer;
 use futures::stream::StreamExt;
-use helium_proto::{services::poc_mobile::CellHeartbeatReqV1, Message};
+use helium_proto::{
+    services::poc_mobile::{CellHeartbeatReqV1, SpeedtestReqV1},
+    Message,
+};
 use std::io;
 use std::path::PathBuf;
+use std::str::FromStr;
 
 /// Print information about a given store file.
 #[derive(Debug, clap::Args)]
@@ -15,12 +19,31 @@ pub struct Cmd {
 impl Cmd {
     pub async fn run(&self) -> Result {
         let mut file_stream = file_source::source(&[&self.in_path]);
-
         let mut wtr = Writer::from_writer(io::stdout());
-        while let Some(result) = file_stream.next().await {
-            let msg = result?;
-            let dec_msg = CellHeartbeatReqV1::decode(msg)?;
-            wtr.serialize(CellHeartbeat::try_from(dec_msg)?)?;
+        let file_prefix = self
+            .in_path
+            .file_name()
+            .expect("invalid_file_name")
+            .to_str()
+            .expect("invalid_file_str")
+            .split('.')
+            .collect::<Vec<_>>()[0];
+
+        match FileType::from_str(file_prefix)? {
+            FileType::CellHeartbeat => {
+                while let Some(result) = file_stream.next().await {
+                    let msg = result?;
+                    let dec_msg = CellHeartbeatReqV1::decode(msg)?;
+                    wtr.serialize(CellHeartbeat::try_from(dec_msg)?)?;
+                }
+            }
+            FileType::CellSpeedtest => {
+                while let Some(result) = file_stream.next().await {
+                    let msg = result?;
+                    let dec_msg = SpeedtestReqV1::decode(msg)?;
+                    wtr.serialize(CellSpeedtest::try_from(dec_msg)?)?;
+                }
+            }
         }
 
         wtr.flush()?;
