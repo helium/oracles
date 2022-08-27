579027248b71d27e5693518ee87fb850d4403806
Matthew Plant <matty@nova-labs.com>
2022-08-26 20:09:22 -0400

    Deb package status server

--- .github/scripts/packagecloud_upload.sh
+++ .github/scripts/packagecloud_upload.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 MOBILE_REWARDS_PKGNAME=$( find target/debian -type f -iname "poc5g-rewards_*.deb" )
 MOBILE_INGEST_PKGNAME=$( find target/debian -type f -iname "poc5g-ingest_*.deb" )
+MOBILE_STATUS_PKGNAME=$( find target/debian -type f -iname "poc5g-status_*.deb" )
 
 curl -u "${PACKAGECLOUD_API_KEY}:" \
      -F "package[distro_version_id]=210" \
@@ -13,4 +14,9 @@ curl -u "${PACKAGECLOUD_API_KEY}:" \
 curl -u "${PACKAGECLOUD_API_KEY}:" \
      -F "package[distro_version_id]=210" \
      -F "package[package_file]=@${MOBILE_INGEST_PKGNAME}" \
+     https://packagecloud.io/api/v1/repos/helium/mobile_ingest/packages.json
+
+curl -u "${PACKAGECLOUD_API_KEY}:" \
+     -F "package[distro_version_id]=210" \
+     -F "package[package_file]=@${MOBILE_STATUS_PKGNAME}" \
      https://packagecloud.io/api/v1/repos/helium/mobile_ingest/packages.json
