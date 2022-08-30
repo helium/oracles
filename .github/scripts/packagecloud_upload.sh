bb76e6b428e04d07c734fa45d6562f498cf43ff6
Jade Allen <jadeallenx@outlook.com>
2022-08-30 18:05:22 -0500

    Add unit files and debian control scripts (#41)
    * Add unit files and debian control scripts
    * Update .github actions
    
    Co-authored-by: Matthew Plant <matty@nova-labs.com>
    Co-authored-by: Jeff Grunewald <jeff.grunewald@gmail.com>

--- .github/scripts/packagecloud_upload.sh
+++ .github/scripts/packagecloud_upload.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 MOBILE_REWARDS_PKGNAME=$( find target/debian -type f -iname "poc5g-rewards_*.deb" )
 MOBILE_INGEST_PKGNAME=$( find target/debian -type f -iname "poc5g-ingest_*.deb" )
+MOBILE_STATUS_PKGNAME=$( find target/debian -type f -iname "poc5g-status_*.deb" )
 
 curl -u "${PACKAGECLOUD_API_KEY}:" \
      -F "package[distro_version_id]=210" \
@@ -14,3 +15,8 @@ curl -u "${PACKAGECLOUD_API_KEY}:" \
      -F "package[distro_version_id]=210" \
      -F "package[package_file]=@${MOBILE_INGEST_PKGNAME}" \
      https://packagecloud.io/api/v1/repos/helium/mobile_ingest/packages.json
+
+curl -u "${PACKAGECLOUD_API_KEY}:" \
+     -F "package[distro_version_id]=210" \
+     -F "package[package_file]=@${MOBILE_STATUS_PKGNAME}" \
+     https://packagecloud.io/api/v1/repos/helium/mobile_status/packages.json
