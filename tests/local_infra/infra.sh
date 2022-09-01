a55220eb46fc8ad3853d4a0690050d4633acfd6c
Jeff Grunewald <jeff.grunewald@gmail.com>
2022-09-01 16:13:27 -0400

    rename poc5g to mobile; expand local testing infra (#40)

--- tests/local_infra/infra.sh
+++ tests/local_infra/infra.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-minio-start() {
+infra-start() {
+    local default_buckets="mobile-ingest mobile-verify mobile-reward iot-ingest iot-verify iot-reward"
+
     if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]];
        then echo "required creds unset"; return 1
     else
         MINIO_ROOT_USER=${MINIO_ROOT_USER:-novaadmin} \
             MINIO_ROOT_PASSWORD=${MINIO_ROOT_PASSWORD:-novaadmin} \
-            MINIO_BUCKETS=${MINIO_BUCKETS:-"poc5g-ingest poc5g-rewards"} \
+            MINIO_BUCKETS=${MINIO_BUCKETS:-"$default_buckets"} \
             docker-compose up -d
     fi
 }
 
-minio-stop() {
+infra-stop() {
     docker-compose down
     if [[ ${1} == "drop" ]];
         then
-            volume=$(docker volume ls | grep bucket-data | awk '{print $2}')
-            docker volume rm $volume
+            volumes=($(docker volume ls | grep -e bucket-data -e db-data | awk '{print $2}'))
+            for vol in "${volumes[@]}"; do docker volume rm $vol; done
     fi
 }
