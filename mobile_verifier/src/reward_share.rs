50e40737b862a8fcf4e8d61f6360f2f6bd73a3cb
Matthew Plant <matty@nova-labs.com>
2022-09-20 18:02:55 -0700

    Remove unused functions

--- mobile_verifier/src/reward_share.rs
+++ mobile_verifier/src/reward_share.rs
@@ -67,18 +67,6 @@ impl OwnerEmissions {
         OwnerEmissions(owner_emissions)
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-
-    pub fn total_emissions(&self) -> Mobile {
-        Mobile::from(
-            self.0
-                .values()
-                .fold(dec!(0), |acc, amt| acc + amt.into_inner()),
-        )
-    }
-
     pub fn into_inner(self) -> HashMap<PublicKey, Mobile> {
         self.0
     }
