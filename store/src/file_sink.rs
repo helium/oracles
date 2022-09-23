1b1ff3de267011399dacb092029a3bda512c0946
Marc Nijdam <marc_nijdam@mac.com>
2022-09-23 08:29:18 -0400

    Use entropy version rename to entropy_report_v1

--- store/src/file_sink.rs
+++ store/src/file_sink.rs
@@ -313,11 +313,12 @@ mod tests {
         let (shutdown_trigger, shutdown_listener) = triggered::trigger();
         let (sender, receiver) = message_channel(10);
 
-        let mut file_sink = FileSinkBuilder::new(FileType::Entropy, &tmp_dir.path(), receiver)
-            .roll_time(chrono::Duration::milliseconds(100))
-            .create()
-            .await
-            .expect("failed to create file sink");
+        let mut file_sink =
+            FileSinkBuilder::new(FileType::EntropyReport, &tmp_dir.path(), receiver)
+                .roll_time(chrono::Duration::milliseconds(100))
+                .create()
+                .await
+                .expect("failed to create file sink");
 
         let sink_thread = tokio::spawn(async move {
             file_sink
@@ -366,6 +367,8 @@ mod tests {
             .file_name()
             .to_str()
             .and_then(|file_name| FileInfo::from_str(file_name).ok())
-            .map_or(false, |file_info| file_info.file_type == FileType::Entropy)
+            .map_or(false, |file_info| {
+                file_info.file_type == FileType::EntropyReport
+            })
     }
 }
