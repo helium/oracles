a619749601463c55240a1ce1dfb4cc0801edcad0
Brian Balser <bbalser@nova-labs.com>
2022-10-19 10:05:43 -0400

    Missed 2 things in create table statement

--- poc_mobile_verifier/migrations/4_heartbeats.sql
+++ poc_mobile_verifier/migrations/4_heartbeats.sql
@@ -3,7 +3,7 @@ drop table heartbeats;
 create table heartbeats (
   hotspot_key bytea not null,
   cbsd_id text not null,
-  reward_weight not null,
+  reward_weight decimal not null,
   timestamp timestamp not null,
-  primary key(pub_key, cbsd_id)
+  primary key(hotspot_key, cbsd_id)
 );
