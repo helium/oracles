0e1a180fae00890dfa7a51b5d57419e4bc914f2b
andymck <andrewb.mckenzie@gmail.com>
2022-10-20 09:26:00 +0100

    Update poc_iot_verifier/src/poc.rs
    Co-authored-by: Rahul Garg <3199183+vihu@users.noreply.github.com>

--- poc_iot_verifier/src/poc.rs
+++ poc_iot_verifier/src/poc.rs
@@ -367,7 +367,7 @@ impl Poc {
         );
         // signal is submitted as DBM * 10
         // min_rcv_signal is plain old DBM
-        if witness.signal / 10 < min_rcv_signal as i32 {
+        if (witness.signal / 10) < min_rcv_signal as i32 {
             tracing::debug!(
                 "witness verification failed, reason: {:?}",
                 InvalidReason::BadRssi
