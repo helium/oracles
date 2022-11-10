270d00ac489f36e6247fd00b332361eadab5ff0f
Rahul Garg <rg@vihu.dev>
2022-11-10 16:08:18 -0700

    Add more info level logging for injector

--- poc_iot_injector/src/server.rs
+++ poc_iot_injector/src/server.rs
@@ -84,7 +84,7 @@ impl Server {
             0,
         ));
         let before_utc = Utc::now();
-        tracing::debug!(
+        tracing::info!(
             "handling poc_tick, after_utc: {:?}, before_utc: {:?}",
             after_utc,
             before_utc
@@ -101,6 +101,7 @@ impl Server {
         )
         .await?;
 
+        tracing::info!("updating meta last_poc_submission_ts to: {:?}", before_utc);
         // NOTE: All the poc_receipt txns for the corresponding after-before period will be
         // submitted above, may take a while to do that but we need to
         // update_last_poc_submission_ts once we're done doing it. This should ensure that we have
@@ -126,6 +127,7 @@ async fn submit_txns(
     let file_list = store
         .list_all(FileType::LoraValidPoc, after_utc, before_utc)
         .await?;
+    tracing::info!("file_list len: {:?}", file_list.len());
 
     let before_ts = before_utc.timestamp_millis();
 
@@ -139,10 +141,13 @@ async fn submit_txns(
                 let txn_details = handle_report_msg(msg, shared_key, before_ts)?;
                 tracing::debug!("txn_details: {:?}", txn_details);
                 if do_submission {
+                    tracing::info!("submitting txn, hash: {:?}", txn_details.hash_b64_url);
                     handle_txn_submission(txn_details.clone(), &mut shared_txn_service).await?;
+                    tracing::info!("storing txn, hash: {:?}", txn_details.hash_b64_url);
                     file_sink_write!("signed_poc_receipt_txn", receipt_sender, txn_details.txn)
                         .await?;
                 } else {
+                    tracing::info!("storing txn, hash: {:?}", txn_details.hash_b64_url);
                     file_sink_write!("signed_poc_receipt_txn", receipt_sender, txn_details.txn)
                         .await?;
                 }
