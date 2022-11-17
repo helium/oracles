0839f649218a324acb74207bf3713fd3bf809617
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2022-11-17 14:48:36 +0000

    reinstate node check

--- poc_iot_verifier/src/loader.rs
+++ poc_iot_verifier/src/loader.rs
@@ -134,7 +134,7 @@ impl Loader {
             FileType::EntropyReport,
         ])
         .map(|file_type| (file_type, shutdown.clone()))
-        .for_each_concurrent(10, |(file_type, shutdown)| async move {
+        .for_each_concurrent(5, |(file_type, shutdown)| async move {
             let _ = self.process_events(*file_type, store, shutdown).await;
         })
         .await;
@@ -259,20 +259,20 @@ impl Loader {
             tracing::debug!("dropping denied gateway : {:?}", &pub_key);
             return false;
         }
-        // if self.check_unknown_gw(pub_key).await {
-        //     tracing::debug!("dropping unknown gateway: {:?}", &pub_key);
-        //     return false;
-        // }
+        if self.check_unknown_gw(pub_key).await {
+            tracing::debug!("dropping unknown gateway: {:?}", &pub_key);
+            return false;
+        }
         true
     }
 
-    // async fn check_unknown_gw(&self, pub_key: &PublicKey) -> bool {
-    //     self.follower_service
-    //         .clone()
-    //         .resolve_gateway_info(pub_key)
-    //         .await
-    //         .is_err()
-    // }
+    async fn check_unknown_gw(&self, pub_key: &PublicKey) -> bool {
+        self.follower_service
+            .clone()
+            .resolve_gateway_info(pub_key)
+            .await
+            .is_err()
+    }
 
     async fn check_gw_denied(&self, pub_key: &PublicKey) -> bool {
         self.deny_list.check_key(pub_key).await
