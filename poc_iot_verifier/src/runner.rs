f32ff9ba7c7ca7e2ba5b2e05e51983ad9ffa127f
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2022-11-30 13:56:36 +0000

    drop concurrency levels

--- poc_iot_verifier/src/runner.rs
+++ poc_iot_verifier/src/runner.rs
@@ -33,7 +33,7 @@ use tokio::time::{self, MissedTickBehavior};
 
 /// the cadence in seconds at which the DB is polled for ready POCs
 const DB_POLL_TIME: time::Duration = time::Duration::from_secs(90);
-const BEACON_WORKERS: usize = 100;
+const BEACON_WORKERS: usize = 75;
 const RUNNER_DB_POOL_SIZE: usize = BEACON_WORKERS * 3;
 
 const WITNESS_REDUNDANCY: u32 = 4;
