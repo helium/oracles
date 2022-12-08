d8fa790352c8681cb06df7137946af20da1237f5
Matthew Plant <maplant95@gmail.com>
2022-12-08 16:47:42 -0500

    Fix and fmt

--- node_follower/src/txn_service.rs
+++ node_follower/src/txn_service.rs
@@ -16,7 +16,9 @@ pub struct TransactionService {
 
 impl TransactionService {
     pub fn from_settings(settings: &Settings) -> Self {
-        Self { client: settings.connect_transactions() }
+        Self {
+            client: settings.connect_transactions(),
+        }
     }
 
     pub async fn submit(&mut self, txn: BlockchainTxn, key: &[u8]) -> Result<TxnSubmitRespV1> {
