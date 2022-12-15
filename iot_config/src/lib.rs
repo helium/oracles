ebec380e411d3976cae8f178f6e5767531f8bde2
jeffgrunewald <jeff.grunewald@gmail.com>
2022-12-15 02:39:56 -0500

    IoT Config project skeleton with stubbed api

---
+++ iot_config/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod gateway_service;
+pub mod org_service;
+pub mod route_service;
+pub mod session_key_service;
+pub mod settings;
+
+pub use gateway_service::GatewayService;
+pub use org_service::OrgService;
+pub use route_service::RouteService;
+pub use session_key_service::SessionKeyFilterService;
+pub use settings::Settings;
+
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{Response, Status};
+
+pub type GrpcResult<T> = Result<Response<T>, Status>;
+pub type GrpcStreamResult<T> = ReceiverStream<Result<T, Status>>;
