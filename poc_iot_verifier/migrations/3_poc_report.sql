5d098e143b8c08a7dd13970a0b1c68f1f7142ad7
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2022-12-16 08:16:35 +0000

    tidy up indexes

--- poc_iot_verifier/migrations/3_poc_report.sql
+++ poc_iot_verifier/migrations/3_poc_report.sql
@@ -23,25 +23,9 @@ create table poc_report (
     created_at timestamptz default now()
 );
 
-
-CREATE INDEX idx_poc_report_id
-ON poc_report(id);
-
 CREATE INDEX idx_poc_report_packet_data
 ON poc_report(packet_data);
 
 CREATE INDEX idx_poc_report_report_type
 ON poc_report(report_type);
-
-CREATE INDEX idx_poc_report_status
-ON poc_report(status);
-
-CREATE INDEX idx_poc_report_created_at
-ON poc_report(created_at);
-
-CREATE INDEX idx_poc_report_attempts
-ON poc_report(attempts);
-
-CREATE INDEX idx_poc_report_report_type_status_created_at
-ON poc_report (report_type,status,created_at);
 
