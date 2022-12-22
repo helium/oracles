74cd1327223bc776fe41598c2fea94ccab2afce5
jeffgrunewald <jeff.grunewald@gmail.com>
2022-12-21 21:55:30 -0500

    create db table migrations

--- iot_config/migrations/2_organizations.sql
+++ iot_config/migrations/2_organizations.sql
@@ -1,5 +1,23 @@
 create table organizations {
-    oui bigserial primary key not null
-    owner_wallet_id text not null
-    payer_wallet_id text not null
-}
+    oui bigserial primary key not null,
+    owner_pubkey text not null,
+    payer_pubkey text not null,
+
+    inserted_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
+};
+
+select trigger updated_at('organizations');
+
+create table organization_devaddr_constraints {
+    oui bigint not null references organizations(oui) on delete cascade,
+    type text not null,
+    nwk_id int not null,
+    start_nwk_addr int not null,
+    end_nwd_addr int not null,
+
+    inserted_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
+};
+
+select trigger updated_at('organization_devaddr_constraints');
