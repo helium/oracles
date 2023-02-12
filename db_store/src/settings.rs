f5f1d36a3461c28b7e224a9cf496f9f9ef33a1fc
Brian Balser <bbalser@nova-labs.com>
2023-02-12 11:01:52 -0500

    Add min_connections, idle_timeout and max_lifetime as optional configuration for database settings

--- db_store/src/settings.rs
+++ db_store/src/settings.rs
@@ -10,6 +10,14 @@ pub struct Settings {
     /// Max open connections to the database. If absent a default is calculated
     /// by application code
     pub max_connections: Option<u32>,
+    /// Min open connection tot he database, if absent the default from sqlx is used
+    pub min_connections: Option<u32>,
+    /// Maximum idle duration for idle connections in seconds, if absent the default from sqlx is
+    /// used.  0 mean infinite timeout
+    pub idle_timeout: Option<u64>,
+    /// Maximum lifetime for connections in seconds, if absent the default from sqlx is
+    /// used.  0 mean infinite timeout
+    pub max_lifetime: Option<u64>,
     /// URL to access the postgres database. For example:
     /// postgres://postgres:postgres@127.0.0.1:5432/mobile_index_db If the url
     /// is not specified the following environment variables are used to pick up
@@ -33,13 +41,45 @@ impl Settings {
         } else {
             PgConnectOptions::new()
         };
-        let pool = PgPoolOptions::new()
-            .max_connections(
-                self.max_connections
-                    .unwrap_or(default_max_connections as u32),
-            )
-            .connect_with(connect_options)
-            .await?;
-        Ok(pool)
+
+        let pool_options = self.pool_options(default_max_connections);
+        Ok(pool_options.connect_with(connect_options).await?)
+    }
+
+    fn pool_options(&self, default_max_connections: usize) -> PgPoolOptions {
+        let mut pool_options = PgPoolOptions::new().max_connections(
+            self.max_connections
+                .unwrap_or(default_max_connections as u32),
+        );
+
+        pool_options = if self.min_connections.is_some() {
+            pool_options.min_connections(self.min_connections.unwrap())
+        } else {
+            pool_options
+        };
+
+        pool_options = if self.idle_timeout.is_some() {
+            let idle_timeout = self.idle_timeout.unwrap();
+            if idle_timeout == 0 {
+                pool_options.idle_timeout(None)
+            } else {
+                let duration = std::time::Duration::from_secs(idle_timeout);
+                pool_options.idle_timeout(duration)
+            }
+        } else {
+            pool_options
+        };
+
+        if self.max_lifetime.is_some() {
+            let max_lifetime = self.max_lifetime.unwrap();
+            if max_lifetime == 0 {
+                pool_options.max_lifetime(None)
+            } else {
+                let duration = std::time::Duration::from_secs(max_lifetime);
+                pool_options.max_lifetime(duration)
+            }
+        } else {
+            pool_options
+        }
     }
 }
