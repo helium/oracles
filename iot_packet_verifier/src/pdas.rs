a298f22e20b4a5c190f220c6b93ad1da915df607
Matthew Plant <maplant95@gmail.com>
2023-02-22 17:16:57 -0500

    Fix delegated_data_credits pda

--- iot_packet_verifier/src/pdas.rs
+++ iot_packet_verifier/src/pdas.rs
@@ -2,16 +2,19 @@
 //! actions on the Solana chain.
 
 use helium_crypto::PublicKeyBinary;
+use sha2::{Digest, Sha256};
 use solana_sdk::pubkey::Pubkey;
 
 /// Returns the PDA for the Delegated Data Credits of the given `payer`.
 pub fn delegated_data_credits(sub_dao: &Pubkey, payer: &PublicKeyBinary) -> Pubkey {
-    let sha_digest = sha256::digest(payer.as_ref());
+    let mut hasher = Sha256::new();
+    hasher.update(&payer.as_ref()[1..]);
+    let sha_digest = hasher.finalize();
     let (ddc_key, _) = Pubkey::find_program_address(
         &[
             "delegated_data_credits".as_bytes(),
             sub_dao.as_ref(),
-            sha_digest.as_bytes(),
+            &sha_digest,
         ],
         &data_credits::ID,
     );
