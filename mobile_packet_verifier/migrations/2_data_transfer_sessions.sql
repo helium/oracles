bcbcfa3e241ca068e87b7c82c64d50ccf225cd3e
Matthew Plant <maplant95@gmail.com>
2023-03-15 14:59:49 -0400

    Fix data_transfer_sessions migration

--- mobile_packet_verifier/migrations/2_data_transfer_sessions.sql
+++ mobile_packet_verifier/migrations/2_data_transfer_sessions.sql
@@ -1,8 +1,8 @@
-CREATE TABLE data_transfer_sesions (
+CREATE TABLE data_transfer_sessions (
        pub_key TEXT NOT NULL,
        payer TEXT NOT NULL,
-       uploaded_bytes BIG INTEGER NOT NULL,
-       downloaded_bytes BIG INTEGER NOT NULL,
+       uploaded_bytes BIGINT NOT NULL,
+       downloaded_bytes BIGINT NOT NULL,
        first_timestamp TIMESTAMPTZ NOT NULL,
        last_timestamp TIMESTAMPTZ NOT NULL,
        PRIMARY KEY(pub_key, payer)
