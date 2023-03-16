4aef2ded142172225240e13ae0760e597e886fbe
Matthew Plant <matty@nova-labs.com>
2023-03-16 13:05:30 -0700

    Mobile Packet Verifier  (#379)
    * Initial work on mobile packet verifier
    
    * Move mobile packet verifier from draft
    
    * Track mobile_session mod
    
    * Fmt
    
    * Switch to daemon instead of using db_lock
    
    * Set auto commit true
    
    * Set default burn period to one hour
    
    * Some fixes
    
    * Fix PDAs and burner
    
    * Fix data_transfer_sessions migration
    
    * Fmt
    
    * Update mobile_packet_verifier/src/verifier.rs
    
    Co-authored-by: Jeff Grunewald <jeff.grunewald@gmail.com>
    
    * Address comments
    
    * Remove unused file
    
    * Another fix
    
    * Revert "Another fix"
    
    This reverts commit 897a97a6e18e473621ea2e87ed711edc15b3e36a.
    
    * Revert "Remove unused file"
    
    This reverts commit d040fee51fd7a368d4ed7ba84d70733695eea811.
    
    * Proper fixes
    
    * Fix accumulate
    
    ---------
    
    Co-authored-by: Jeff Grunewald <jeff.grunewald@gmail.com>

---
+++ mobile_packet_verifier/src/accumulate.rs
@@ -0,0 +1,48 @@
+use chrono::{DateTime, Utc};
+use file_store::mobile_session::DataTransferSessionIngestReport;
+use futures::{Stream, StreamExt};
+use sqlx::{Postgres, Transaction};
+
+#[derive(thiserror::Error, Debug)]
+pub enum AccumulationError {
+    #[error("file store error: {0}")]
+    FileStoreError(#[from] file_store::Error),
+    #[error("sqlx error: {0}")]
+    SqlxError(#[from] sqlx::Error),
+    #[error("reports stream dropped")]
+    ReportsStreamDropped,
+}
+
+pub async fn accumulate_sessions(
+    conn: &mut Transaction<'_, Postgres>,
+    curr_file_ts: DateTime<Utc>,
+    reports: impl Stream<Item = DataTransferSessionIngestReport>,
+) -> Result<(), AccumulationError> {
+    tokio::pin!(reports);
+
+    while let Some(DataTransferSessionIngestReport { report, .. }) = reports.next().await {
+        if report.reward_cancelled {
+            continue;
+        }
+        let event = report.data_transfer_usage;
+        sqlx::query(
+            r#"
+            INSERT INTO data_transfer_sessions (pub_key, payer, uploaded_bytes, downloaded_bytes, first_timestamp, last_timestamp)
+            VALUES ($1, $2, $3, $4, $5, $5)
+            ON CONFLICT (pub_key, payer) DO UPDATE SET
+            uploaded_bytes = data_transfer_sessions.uploaded_bytes + EXCLUDED.uploaded_bytes,
+            downloaded_bytes = data_transfer_sessions.downloaded_bytes + EXCLUDED.downloaded_bytes,
+            last_timestamp = MAX(data_transfer_sessions.last_timestamp, EXCLUDED.last_timestamp)
+            "#
+        )
+            .bind(event.pub_key)
+            .bind(event.payer)
+            .bind(event.upload_bytes as i64)
+            .bind(event.download_bytes as i64)
+            .bind(curr_file_ts)
+            .execute(&mut *conn)
+            .await?;
+    }
+
+    Ok(())
+}
---
+++ mobile_packet_verifier/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod accumulate;
+pub mod burner;
+pub mod daemon;
+mod pdas;
+pub mod settings;
