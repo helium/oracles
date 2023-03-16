8b1c8c5a494e5079e7b00804d2b82b8c7254c771
Matthew Plant <maplant95@gmail.com>
2023-03-16 13:35:11 -0400

    Revert "Another fix"
    This reverts commit 897a97a6e18e473621ea2e87ed711edc15b3e36a.

--- file_store/src/lib.rs
+++ file_store/src/lib.rs
@@ -12,6 +12,7 @@ pub mod iot_invalid_poc;
 pub mod iot_packet;
 pub mod iot_valid_poc;
 pub mod iot_witness_report;
+pub mod mobile_session;
 pub mod reward_manifest;
 mod settings;
 pub mod speedtest;
