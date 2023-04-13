464ac18ad36e73ce1c33f1fb07170322f8d05c53
Brian Balser <brian.balser@gmail.com>
2023-04-13 17:02:00 -0400

    Update mobile_config to not return join_handle (#450)

--- mobile_verifier/src/cli/server.rs
+++ mobile_verifier/src/cli/server.rs
@@ -93,7 +93,7 @@ impl Cmd {
 
         let reward_period_hours = settings.rewards;
         let verifications_per_period = settings.verifications;
-        let (config_client, config_client_handle) = Client::from_settings(&settings.config_client)?;
+        let config_client = Client::from_settings(&settings.config_client)?;
         let file_store = FileStore::from_settings(&settings.ingest).await?;
 
         let (price_tracker, tracker_process) =
@@ -117,7 +117,6 @@ impl Cmd {
 
         tokio::try_join!(
             db_join_handle.map_err(Error::from),
-            config_client_handle.map_err(Error::from),
             heartbeats_server
                 .run(&shutdown_listener)
                 .map_err(Error::from),
