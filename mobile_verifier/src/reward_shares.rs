fb0248b6d2cad1fae083097defa0fe5b76743903
Matthew Plant <maplant95@gmail.com>
2023-04-20 15:58:45 -0400

    Use .take

--- mobile_verifier/src/reward_shares.rs
+++ mobile_verifier/src/reward_shares.rs
@@ -177,8 +177,7 @@ impl PocShares {
                         let start_period = epoch.start.encode_timestamp();
                         let end_period = epoch.end.encode_timestamp();
                         let poc_reward = poc_rewards_per_share * amount;
-                        let dc_transfer_reward =
-                            std::mem::take(&mut dc_transfer_reward).unwrap_or(Decimal::ZERO);
+                        let dc_transfer_reward = dc_transfer_reward.take().unwrap_or(Decimal::ZERO);
                         let hotspot_key: Vec<u8> = hotspot_key.clone().into();
                         let radio_reward_share = proto::RadioRewardShare {
                             owner_key: Vec::new(),
