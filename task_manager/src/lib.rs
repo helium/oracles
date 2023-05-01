715a0b364c32c8c5b7940d9396651d1949331853
Brian Balser <bbalser@nova-labs.com>
2023-04-30 21:27:04 -0400

    Add test and some refactoring

--- task_manager/src/lib.rs
+++ task_manager/src/lib.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code, unused)]
 
 use futures::{
-    future::{BoxFuture, Shared},
-    StreamExt,
+    future::{self, BoxFuture, Shared},
+    Future, StreamExt, TryFutureExt,
 };
 use tokio::{
     signal,
     sync::{mpsc, oneshot},
+    task::JoinHandle,
 };
 use tokio_util::sync::CancellationToken;
 
@@ -15,6 +16,10 @@ pub trait ManagedTask {
 }
 
 pub struct TaskManager {
+    tasks: Vec<Box<dyn ManagedTask>>,
+}
+
+pub struct TaskManagerBuilder {
     tasks: Vec<Box<dyn ManagedTask>>,
 }
 
@@ -28,88 +33,119 @@ impl TaskManager {
         Self { tasks: Vec::new() }
     }
 
+    pub fn builder() -> TaskManagerBuilder {
+        TaskManagerBuilder { tasks: Vec::new() }
+    }
+
     pub fn add(&mut self, task: impl ManagedTask + 'static) {
         self.tasks.push(Box::new(task));
     }
 
     pub async fn start(self) -> anyhow::Result<()> {
         let (stop_sender, mut stop_receiver) = mpsc::channel(1);
-        let (message_sender, mut message_receiver) = mpsc::channel::<Messages>(5);
+        let (message_sender, mut message_receiver) = mpsc::channel(5);
         register_signal_listeners(stop_sender);
 
         let mut tokens = create_tokens(self.tasks.len());
 
-        let mut futures: Vec<_> = tokens
-            .clone()
-            .into_iter()
-            .zip(self.tasks.into_iter())
-            .map(|(token, task)| task.start(token))
-            .collect();
+        let mut futures = start_futures(tokens.clone(), self.tasks);
+        let handle = tokio::spawn(async move { handle_futures(futures, message_sender).await });
 
-        let handle = tokio::spawn(async move {
-            let mut select_all = futures::future::select_all(futures.into_iter());
-            let mut last_result: Option<anyhow::Result<()>> = None;
+        tokio::spawn(async move { handle_stopping(tokens, stop_receiver, message_receiver).await });
 
-            loop {
-                let (result, index, remaining) = select_all.await;
-                match result {
-                    Ok(_) => {
-                        message_sender.send(Messages::TaskStopped(index)).await;
-                    }
-                    Err(err) => {
-                        message_sender.send(Messages::StopAll(index)).await;
-                        if last_result.is_none() {
-                            last_result = Some(Err(err));
-                        }
-                    }
-                }
+        handle
+            .await
+            .map_err(|err| err.into())
+            .and_then(|result| result)
+    }
+}
 
-                if remaining.len() == 0 {
-                    break;
-                } else {
-                    select_all = futures::future::select_all(remaining.into_iter());
+impl TaskManagerBuilder {
+    pub fn add(mut self, task: impl ManagedTask + 'static) -> Self {
+        self.tasks.push(Box::new(task));
+        self
+    }
+
+    pub fn start(self) -> impl Future<Output = anyhow::Result<()>> {
+        let manager = TaskManager { tasks: self.tasks };
+        manager.start()
+    }
+}
+
+async fn handle_futures(
+    futures: Vec<BoxFuture<'static, anyhow::Result<()>>>,
+    sender: mpsc::Sender<Messages>,
+) -> anyhow::Result<()> {
+    let mut select_all = future::select_all(futures.into_iter());
+    let mut last_result: Option<anyhow::Result<()>> = None;
+
+    loop {
+        let (result, index, remaining) = select_all.await;
+        match result {
+            Ok(_) => {
+                sender.send(Messages::TaskStopped(index)).await;
+            }
+            Err(err) => {
+                sender.send(Messages::StopAll(index)).await;
+                if last_result.is_none() {
+                    last_result = Some(Err(err));
                 }
             }
+        }
 
-            last_result.unwrap_or(Ok(()))
-        });
-
-        tokio::spawn(async move {
-            let mut stopping = false;
-            loop {
-                if tokens.len() == 0 {
-                    break;
-                }
-
-                if stopping {
-                    if let Some(token) = tokens.last() {
-                        token.cancel();
-                    }
-                }
-
-                tokio::select! {
-                    _ = stop_receiver.recv() => {
-                        stopping = true;
-                    }
-                    Some(msg) = message_receiver.recv() => match msg {
-                        Messages::TaskStopped(index) => {
-                            tokens.remove(index);
-                        }
-                        Messages::StopAll(index) => {
-                            tokens.remove(index);
-                            stopping = true;
-                        }
-                    },
-                }
-            }
-        });
-
-        match handle.await {
-            Ok(Ok(_)) => Ok(()),
-            Ok(Err(err)) => Err(err),
-            Err(err) => Err(err.into()),
+        if remaining.len() == 0 {
+            break;
+        } else {
+            select_all = future::select_all(remaining.into_iter());
         }
     }
+
+    last_result.unwrap_or(Ok(()))
+}
+
+async fn handle_stopping(
+    mut tokens: Vec<CancellationToken>,
+    mut stop_receiver: mpsc::Receiver<bool>,
+    mut receiver: mpsc::Receiver<Messages>,
+) {
+    let mut stopping = false;
+    loop {
+        if tokens.len() == 0 {
+            break;
+        }
+
+        if stopping {
+            if let Some(token) = tokens.last() {
+                token.cancel();
+            }
+        }
+
+        tokio::select! {
+            _ = stop_receiver.recv() => {
+                stopping = true;
+            }
+            Some(msg) = receiver.recv() => match msg {
+                Messages::TaskStopped(index) => {
+                    tokens.remove(index);
+                }
+                Messages::StopAll(index) => {
+                    tokens.remove(index);
+                    stopping = true;
+                }
+            },
+        }
+    }
+}
+
+fn start_futures(
+    tokens: Vec<CancellationToken>,
+    tasks: Vec<Box<dyn ManagedTask>>,
+) -> Vec<BoxFuture<'static, anyhow::Result<()>>> {
+    tokens
+        .into_iter()
+        .zip(tasks.into_iter())
+        .map(|(token, task)| task.start(token))
+        .collect()
 }
 
 fn register_signal_listeners(sender: mpsc::Sender<bool>) {
@@ -130,4 +166,129 @@ fn create_tokens(n: usize) -> Vec<CancellationToken> {
         result.push(CancellationToken::new());
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::anyhow;
+
+    struct TestTask {
+        id: u64,
+        delay: u64,
+        result: anyhow::Result<()>,
+        sender: mpsc::Sender<u64>,
+    }
+
+    impl ManagedTask for TestTask {
+        fn start(
+            self: Box<Self>,
+            token: CancellationToken,
+        ) -> BoxFuture<'static, anyhow::Result<()>> {
+            let handle = tokio::spawn(async move {
+                tokio::select! {
+                    _ = token.cancelled() => (),
+                    _ = tokio::time::sleep(std::time::Duration::from_millis(self.delay)) => (),
+                }
+
+                self.sender.send(self.id).await;
+                self.result
+            });
+
+            Box::pin(
+                handle
+                    .map_err(|err| err.into())
+                    .and_then(|result| async move { result }),
+            )
+        }
+    }
+
+    #[tokio::test]
+    async fn stop_when_all_tasks_have_completed() {
+        let (sender, mut receiver) = mpsc::channel(5);
+
+        let result = TaskManager::builder()
+            .add(TestTask {
+                id: 1,
+                delay: 50,
+                result: Ok(()),
+                sender: sender.clone(),
+            })
+            .add(TestTask {
+                id: 2,
+                delay: 100,
+                result: Ok(()),
+                sender: sender.clone(),
+            })
+            .start()
+            .await;
+
+        assert_eq!(Some(1), receiver.recv().await);
+        assert_eq!(Some(2), receiver.recv().await);
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn will_stop_all_in_reverse_order_after_error() {
+        let (sender, mut receiver) = mpsc::channel(5);
+
+        let result = TaskManager::builder()
+            .add(TestTask {
+                id: 1,
+                delay: 1000,
+                result: Ok(()),
+                sender: sender.clone(),
+            })
+            .add(TestTask {
+                id: 2,
+                delay: 50,
+                result: Err(anyhow!("error")),
+                sender: sender.clone(),
+            })
+            .add(TestTask {
+                id: 3,
+                delay: 1000,
+                result: Ok(()),
+                sender: sender.clone(),
+            })
+            .start()
+            .await;
+
+        assert_eq!(Some(2), receiver.recv().await);
+        assert_eq!(Some(3), receiver.recv().await);
+        assert_eq!(Some(1), receiver.recv().await);
+        assert_eq!("error", result.unwrap_err().to_string());
+    }
+
+    #[tokio::test]
+    async fn will_return_first_error_returned() {
+        let (sender, mut receiver) = mpsc::channel(5);
+
+        let result = TaskManager::builder()
+            .add(TestTask {
+                id: 1,
+                delay: 1000,
+                result: Ok(()),
+                sender: sender.clone(),
+            })
+            .add(TestTask {
+                id: 2,
+                delay: 50,
+                result: Err(anyhow!("error")),
+                sender: sender.clone(),
+            })
+            .add(TestTask {
+                id: 3,
+                delay: 200,
+                result: Err(anyhow!("second")),
+                sender: sender.clone(),
+            })
+            .start()
+            .await;
+
+        assert_eq!(Some(2), receiver.recv().await);
+        assert_eq!(Some(3), receiver.recv().await);
+        assert_eq!(Some(1), receiver.recv().await);
+        assert_eq!("error", result.unwrap_err().to_string());
+    }
 }
