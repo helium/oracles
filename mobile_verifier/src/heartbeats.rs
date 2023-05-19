bbf15a8bfa162e62583f2f356536de714dfe477c
Brian Balser <bbalser@nova-labs.com>
2023-05-19 07:50:27 -0400

    Update validated epoch to be 3hours in past and 30 minutes in future from file timestamp

--- mobile_verifier/src/heartbeats.rs
+++ mobile_verifier/src/heartbeats.rs
@@ -97,8 +97,8 @@ impl HeartbeatDaemon {
         file: FileInfoStream<CellHeartbeatIngestReport>,
         cache: &Arc<Cache<(String, DateTime<Utc>), ()>>,
     ) -> anyhow::Result<()> {
-        let epoch = (file.file_info.timestamp - Duration::hours(1))
-            ..(file.file_info.timestamp + Duration::hours(1));
+        let epoch = (file.file_info.timestamp - Duration::hours(3))
+            ..(file.file_info.timestamp + Duration::hours(30));
         let mut transaction = self.pool.begin().await?;
         let reports = file.into_stream(&mut transaction).await?;
 
