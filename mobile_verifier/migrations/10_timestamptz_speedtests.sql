d74bf4384a2740d1da66eb566cb51de9270f6bca
Matthew Plant <maplant95@gmail.com>
2023-06-07 11:47:59 -0400

    Don't drop speedtests, migrate data

--- mobile_verifier/migrations/10_timestamptz_speedtests.sql
+++ mobile_verifier/migrations/10_timestamptz_speedtests.sql
@@ -1,15 +1,21 @@
-DROP TABLE speedtests;
-DROP TYPE speedtest;
-
-CREATE TYPE speedtest AS (
+CREATE TYPE speedtest_migration AS (
        timestamp TIMESTAMPTZ,
        upload_speed BIGINT,
        download_speed BIGINT,
        latency INTEGER
 );
 
-CREATE TABLE speedtests (
-       id TEXT PRIMARY KEY NOT NULL,
-       speedtests speedtest[] NOT NULL,
-       latest_timestamp TIMESTAMPTZ NOT NULL
-);
+CREATE FUNCTION tomigration(speedtest)
+       RETURNS speedtest_migration
+       STRICT IMMUTABLE LANGUAGE SQL AS
+$$ SELECT CAST ( ROW ( ($1).timestamp at time zone 'UTC', ($1).upload_speed, ($1).download_speed, ($1).latency ) AS speedtest_migration ); $$ ;
+
+CREATE CAST ( speedtest as speedtest_migration ) WITH FUNCTION tomigration(speedtest) AS IMPLICIT;
+
+ALTER TABLE speedtests ALTER COLUMN speedtests TYPE speedtest_migration [] ;
+ALTER TABLE speedtests ALTER COLUMN latest_timestamp TYPE TIMESTAMPTZ USING latest_timestamp at time zone 'UTC' ;
+
+DROP CAST ( speedtest as speedtest_migration );
+DROP FUNCTION tomigration(speedtest);
+DROP TYPE speedtest;
+ALTER TYPE speedtest_migration RENAME TO speedtest;
