c37f583c2bd9fb73d26848c87caf5c25b5ff2aa8
Matthew Plant <maplant95@gmail.com>
2023-07-25 17:39:25 -0400

    Go back to modeled coverage migration

--- mobile_verifier/migrations/15_heartbeat_coverage_object.sql
+++
@@ -1,2 +0,0 @@
--- Coverage object can be NULL
-ALTER TABLE heartbeats ADD COLUMN coverage_object UUID;
---
+++ mobile_verifier/migrations/15_modeled_coverage.sql
@@ -0,0 +1,28 @@
+CREATE TYPE signal_level AS ENUM (
+       'none',
+       'low',
+       'medium',
+       'high'
+);
+
+CREATE TABLE hex_coverage (
+       uuid UUID NOT NULL,
+       hex BIGINT NOT NULL,
+       indoor BOOLEAN NOT NULL,
+       cbsd_id TEXT NOT NULL,
+       signal_level signal_level NOT NULL,
+       coverage_claim_time TIMESTAMPTZ NOT NULL,
+       inserted_at TIMESTAMPTZ NOT NULL,
+       PRIMARY KEY (uuid, hex)
+);
+
+CREATE TABLE seniority (
+       cbsd_id TEXT NOT NULL,
+       seniority_ts TIMESTAMPTZ NOT NULL,
+       last_heartbeat TIMESTAMPTZ NOT NULL,
+       uuid UUID NOT NULL,
+       PRIMARY KEY (cbsd_id, seniority_ts)
+);
+
+-- Coverage object can be NULL
+ALTER TABLE heartbeats ADD COLUMN coverage_object UUID;
