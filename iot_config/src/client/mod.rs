96209c3a414bf0b77d5e49e8675752a23861dd70
Jeff Grunewald <jeff.grunewald@gmail.com>
2023-08-21 13:06:20 -0400

    retry iot_config client rpcs (#607)
    * retry iot_config client rpcs
    
    * extend retry sleep to full seconds

--- iot_config/src/client/mod.rs
+++ iot_config/src/client/mod.rs
@@ -35,6 +35,34 @@ pub struct Client {
     batch_size: u32,
 }
 
+macro_rules! call_with_retry {
+    ($rpc:expr) => {{
+        use tonic::Code;
+
+        let mut attempt = 1;
+        loop {
+            let response = $rpc.await;
+            match response {
+                Ok(resp) => break Ok(resp),
+                Err(status) => match status.code() {
+                    Code::Cancelled | Code::DeadlineExceeded | Code::Unavailable => {
+                        if attempt < 3 {
+                            attempt += 1;
+                            tokio::time::sleep(Duration::from_secs(attempt)).await;
+                            continue;
+                        } else {
+                            break Err(status);
+                        }
+                    }
+                    _ => break Err(status),
+                },
+            }
+        }
+    }};
+}
+
+pub(crate) use call_with_retry;
+
 impl Client {
     pub fn from_settings(settings: &Settings) -> Result<Self, Box<helium_crypto::Error>> {
         let channel = Endpoint::from(settings.url.clone())
@@ -60,7 +88,8 @@ impl Client {
             signature: vec![],
         };
         request.signature = self.signing_key.sign(&request.encode_to_vec())?;
-        let response = self.admin_client.region_params(request).await?.into_inner();
+        let response =
+            call_with_retry!(self.admin_client.region_params(request.clone()))?.into_inner();
         response.verify(&self.config_pubkey)?;
         Ok(RegionParamsInfo {
             region: response.region(),
@@ -87,7 +116,7 @@ impl gateway_info::GatewayInfoResolver for Client {
         };
         request.signature = self.signing_key.sign(&request.encode_to_vec())?;
         tracing::debug!(pubkey = address.to_string(), "fetching gateway info");
-        let response = match self.gateway_client.info(request).await {
+        let response = match call_with_retry!(self.gateway_client.info(request.clone())) {
             Ok(info_resp) => {
                 let response = info_resp.into_inner();
                 response.verify(&self.config_pubkey)?;
@@ -110,10 +139,7 @@ impl gateway_info::GatewayInfoResolver for Client {
         request.signature = self.signing_key.sign(&request.encode_to_vec())?;
         tracing::debug!("fetching gateway info stream");
         let pubkey = Arc::new(self.config_pubkey.clone());
-        let response_stream = self
-            .gateway_client
-            .info_stream(request)
-            .await?
+        let response_stream = call_with_retry!(self.gateway_client.info_stream(request.clone()))?
             .into_inner()
             .filter_map(|resp| async move { resp.ok() })
             .map(move |resp| (resp, pubkey.clone()))
