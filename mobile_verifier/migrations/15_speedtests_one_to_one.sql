28fc25980f6bdc2539e65f4025045159dbafad59
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2023-08-23 15:55:35 +0100

    fix accidently pushed changes

--- mobile_verifier/migrations/15_speedtests_one_to_one.sql
+++ mobile_verifier/migrations/15_speedtests_one_to_one.sql
@@ -4,16 +4,16 @@ CREATE TABLE speedtests_migration (
        upload_speed bigint,
        download_speed bigint,
        latency integer,
-       "serial" text,
+       serial_num text,
        timestamp timestamptz NOT NULL,
        inserted_at timestamptz default now(),
        PRIMARY KEY(pubkey, timestamp)
 );
-CREATE INDEX idx_speedtests2_pubkey on speedtests_migration (pubkey);
+CREATE INDEX idx_speedtests_pubkey on speedtests_migration (pubkey);
 
-INSERT INTO speedtests_migration (pubkey, upload_speed, download_speed, latency, serial, timestamp)
+INSERT INTO speedtests_migration (pubkey, upload_speed, download_speed, latency, serial_num, timestamp)
 SELECT id, (st).upload_speed, (st).download_speed, (st).latency, '', (st).timestamp
-FROM (select id, unnest(speedtests) as st from speedtests_old) as tmp
+FROM (select id, unnest(speedtests) as st from speedtests) as tmp
 ON CONFLICT DO NOTHING;
 
 ALTER TABLE speedtests RENAME TO speedtests_old;
