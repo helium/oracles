ee9472f63e6d04b23ec9f98bd69e2c1091669a8e
Brian Balser <bbalser@nova-labs.com>
2023-12-15 12:38:14 -0500

    Add process_name to file_info_poller

--- file_store/src/file_info_poller.rs
+++ file_store/src/file_info_poller.rs
@@ -1,5 +1,5 @@
 use crate::{traits::MsgDecode, Error, FileInfo, FileStore, Result};
-use chrono::{DateTime, Duration, TimeZone, Utc};
+use chrono::{DateTime, Duration, Utc};
 use derive_builder::Builder;
 use futures::{future::LocalBoxFuture, stream::BoxStream, StreamExt, TryFutureExt};
 use retainer::Cache;
@@ -17,6 +17,7 @@ type MemoryFileCache = Cache<String, bool>;
 
 pub struct FileInfoStream<T> {
     pub file_info: FileInfo,
+    process_name: String,
     stream: BoxStream<'static, T>,
 }
 
@@ -24,15 +25,19 @@ impl<T> FileInfoStream<T>
 where
     T: Send,
 {
-    pub fn new(file_info: FileInfo, stream: BoxStream<'static, T>) -> Self {
-        Self { file_info, stream }
+    pub fn new(process_name: String, file_info: FileInfo, stream: BoxStream<'static, T>) -> Self {
+        Self {
+            file_info,
+            process_name,
+            stream,
+        }
     }
 
     pub async fn into_stream(
         self,
         transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
     ) -> Result<BoxStream<'static, T>> {
-        db::insert(transaction, self.file_info).await?;
+        db::insert(transaction, &self.process_name, self.file_info).await?;
         Ok(self.stream)
     }
 }
@@ -56,6 +61,8 @@ pub struct FileInfoPollerConfig<T> {
     offset: Duration,
     #[builder(default = "20")]
     queue_size: usize,
+    #[builder(default = r#""default".to_string()"#)]
+    process_name: String,
     #[builder(setter(skip))]
     p: PhantomData<T>,
 }
@@ -117,9 +124,19 @@ where
         let cache = create_cache();
         let mut poll_trigger = tokio::time::interval(self.poll_duration());
         let mut cleanup_trigger = tokio::time::interval(CLEAN_DURATION);
+        let process_name = self.config.process_name.clone();
 
-        let mut latest_ts = db::latest_ts(&self.config.db, &self.config.prefix).await?;
-        tracing::info!(r#type = self.config.prefix, "starting FileInfoPoller",);
+        let mut latest_ts = db::latest_ts(
+            &self.config.db,
+            &self.config.process_name,
+            &self.config.prefix,
+        )
+        .await?;
+        tracing::info!(
+            r#type = self.config.prefix,
+            %process_name,
+            "starting FileInfoPoller",
+        );
 
         loop {
             let after = self.after(latest_ts);
@@ -128,19 +145,19 @@ where
             tokio::select! {
                 biased;
                 _ = shutdown.clone() => {
-                    tracing::info!(r#type = self.config.prefix, "stopping FileInfoPoller");
+                    tracing::info!(r#type = self.config.prefix, %process_name, "stopping FileInfoPoller");
                     break;
                 }
                 _ = cleanup_trigger.tick() => self.clean(&cache).await?,
                 _ = poll_trigger.tick() => {
                     let files = self.config.store.list_all(&self.config.prefix, after, before).await?;
                     for file in files {
-                        if !is_already_processed(&self.config.db, &cache, &file).await? {
-                            if send_stream(&self.sender, &self.config.store, file.clone()).await? {
+                        if !is_already_processed(&self.config.db, &cache, &process_name, &file).await? {
+                            if send_stream(&self.sender, &self.config.store, process_name.clone(), file.clone()).await? {
                                 latest_ts = Some(file.timestamp);
                                 cache_file(&cache, &file).await;
                             } else {
-                                tracing::info!("FileInfoPoller: channel full");
+                                tracing::info!(r#type = self.config.prefix, %process_name, "FileInfoPoller: channel full");
                                 break;
                             }
                         }
@@ -164,7 +181,12 @@ where
 
     async fn clean(&self, cache: &MemoryFileCache) -> Result {
         cache.purge(4, 0.25).await;
-        db::clean(&self.config.db, &self.config.prefix).await?;
+        db::clean(
+            &self.config.db,
+            &self.config.process_name,
+            &self.config.prefix,
+        )
+        .await?;
         Ok(())
     }
 
@@ -179,6 +201,7 @@ where
 async fn send_stream<T>(
     sender: &Sender<FileInfoStream<T>>,
     store: &FileStore,
+    process_name: String,
     file: FileInfo,
 ) -> Result<bool>
 where
@@ -210,11 +233,7 @@ where
         })
         .boxed();
 
-    let incoming_data_stream = FileInfoStream {
-        file_info: file,
-        stream,
-    };
-
+    let incoming_data_stream = FileInfoStream::new(process_name, file, stream);
     match sender.try_send(incoming_data_stream) {
         Ok(_) => Ok(true),
         Err(TrySendError::Full(_)) => Ok(false),
@@ -229,12 +248,13 @@ fn create_cache() -> MemoryFileCache {
 async fn is_already_processed(
     db: impl sqlx::PgExecutor<'_>,
     cache: &MemoryFileCache,
+    process_name: &str,
     file_info: &FileInfo,
 ) -> Result<bool> {
     if cache.get(&file_info.key).await.is_some() {
         Ok(true)
     } else {
-        db::exists(db, file_info).await
+        db::exists(db, process_name, file_info).await
     }
 }
 
@@ -247,70 +267,100 @@ mod db {
 
     pub async fn latest_ts(
         db: impl sqlx::PgExecutor<'_>,
+        process_name: &str,
         file_type: &str,
     ) -> Result<Option<DateTime<Utc>>> {
-        let default = Utc.timestamp_opt(0, 0).single().unwrap();
-
-        let result = sqlx::query_scalar::<_, DateTime<Utc>>(
+        sqlx::query_scalar::<_, Option<DateTime<Utc>>>(
             r#"
-        SELECT COALESCE(MAX(file_timestamp), $1) FROM files_processed where file_type = $2
-        "#,
-        )
-        .bind(default)
-        .bind(file_type)
-        .fetch_one(db)
-        .await?;
-
-        if result == default {
-            Ok(None)
-        } else {
-            Ok(Some(result))
-        }
+                SELECT MAX(file_timestamp) FROM files_processed where process_name = $1 and file_type = $2
+            "#,
+            )
+            .bind(process_name)
+            .bind(file_type)
+            .fetch_one(db)
+            .await
+            .map_err(Error::from)
     }
 
-    pub async fn exists(db: impl sqlx::PgExecutor<'_>, file_info: &FileInfo) -> Result<bool> {
-        Ok(sqlx::query_scalar::<_, bool>(
+    pub async fn exists(
+        db: impl sqlx::PgExecutor<'_>,
+        process_name: &str,
+        file_info: &FileInfo,
+    ) -> Result<bool> {
+        sqlx::query_scalar::<_, bool>(
             r#"
-        SELECT EXISTS(SELECT 1 from files_processed where file_name = $1)
-        "#,
-        )
-        .bind(file_info.key.clone())
-        .fetch_one(db)
-        .await?)
+                SELECT EXISTS(SELECT 1 from files_processed where process_name = $1 and file_name = $2)
+            "#,
+            )
+            .bind(process_name)
+            .bind(&file_info.key)
+            .fetch_one(db)
+            .await
+            .map_err(Error::from)
     }
 
     pub async fn insert(
         tx: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+        process_name: &str,
         file_info: FileInfo,
     ) -> Result {
-        sqlx::query(r#"
-        INSERT INTO files_processed(file_name, file_type, file_timestamp, processed_at) VALUES($1, $2, $3, $4)
-        "#)
-    .bind(file_info.key)
-    .bind(&file_info.prefix)
-    .bind(file_info.timestamp)
-    .bind(Utc::now())
-    .execute(tx)
-    .await?;
-
-        Ok(())
-    }
-
-    pub async fn clean(db: impl sqlx::PgExecutor<'_>, file_type: &str) -> Result {
         sqlx::query(
             r#"
-        DELETE FROM files_processed where file_name in (
-            SELECT file_name
-            FROM files_processed
-            WHERE file_type = $1
-            ORDER BY file_timestamp DESC
-            OFFSET 100
+                INSERT INTO files_processed(process_name, file_name, file_type, file_timestamp, processed_at) VALUES($1, $2, $3, $4, $5)
+            "#)
+            .bind(process_name)
+            .bind(file_info.key)
+            .bind(&file_info.prefix)
+            .bind(file_info.timestamp)
+            .bind(Utc::now())
+            .execute(tx)
+            .await
+            .map(|_| ())
+            .map_err(Error::from)
+    }
+
+    pub async fn clean(
+        db: impl sqlx::PgExecutor<'_>,
+        process_name: &str,
+        file_type: &str,
+    ) -> Result {
+        sqlx::query(
+            r#"
+                DELETE FROM files_processed where file_name in (
+                    SELECT file_name
+                    FROM files_processed
+                    WHERE process_name = $1 and file_type = $2
+                    ORDER BY file_timestamp DESC
+                    OFFSET 100
+                )
+            "#,
         )
-        "#,
-        )
+        .bind(process_name)
         .bind(file_type)
         .execute(db)
-        .await?;
+        .await
+        .map(|_| ())
+        .map_err(Error::from)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sqlx::postgres::PgPoolOptions;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn brian() -> anyhow::Result<()> {
+        let pool = PgPoolOptions::new()
+            .max_connections(1)
+            .connect("postgres://postgres:postgres@localhost/mobile_verifier")
+            .await?;
+
+        let found = db::latest_ts(&pool, "default", "wifi_heartbeat_report").await?;
+        dbg!(found);
+        let not_found = db::latest_ts(&pool, "default", "wifi_heartbeat_report2").await?;
+        dbg!(not_found);
 
         Ok(())
     }
