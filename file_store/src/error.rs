154535a4e51139f4bc86e0a05d6bf8ddffebb317
Evgeny Shevchenko <96988001+thehardbits@users.noreply.github.com>
2024-03-01 20:31:25 +0100

    add ExternalError variant to file_store::error::Error (#748)
    * add ExternalError variant to file_store::error::Error
    
    * Naming fix, add comments
    
    * cargo fmt fix

--- file_store/src/error.rs
+++ file_store/src/error.rs
@@ -37,6 +37,10 @@ pub enum Error {
     DbError(#[from] sqlx::Error),
     #[error("channel send error")]
     SendError(#[from] tokio::sync::mpsc::error::SendError<()>),
+    //Generic error wrapper for external (out of that repository) traits implementations.
+    //Not recommended for internal use!
+    #[error("external error")]
+    ExternalError(#[from] Box<dyn std::error::Error + Send + Sync>),
 }
 
 #[derive(Error, Debug)]
