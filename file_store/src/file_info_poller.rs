88d80fe0045b3d32fe55d47844f207842c2be2d3
jeffgrunewald <jeff.grunewald@gmail.com>
2024-03-31 23:38:59 -0400

    record processing latency for file info streams

--- file_store/src/file_info_poller.rs
+++ file_store/src/file_info_poller.rs
@@ -59,6 +59,13 @@ where
     where
         T: 'static,
     {
+        let latency = Utc::now() - self.file_info.timestamp;
+        metrics::histogram!(
+            "file-processing-latency",
+            latency.num_seconds() as f64,
+            "file-type" => self.file_info.prefix.clone()
+        );
+
         recorder.record(&self.process_name, &self.file_info).await?;
         Ok(futures::stream::iter(self.data.into_iter()).boxed())
     }
