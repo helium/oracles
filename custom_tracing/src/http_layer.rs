d9c12e9a7990390d4ec4a8dc23747237d45b18c5
Macpie <pierre@nova-labs.com>
2024-04-18 11:35:54 -0700

    Allow layers to take a function that create a span

--- custom_tracing/src/http_layer.rs
+++ custom_tracing/src/http_layer.rs
@@ -7,9 +7,9 @@ use tower_http::{
     LatencyUnit,
 };
 use tower_layer::Layer;
-use tracing::Level;
+use tracing::{Level, Span};
 
-pub fn new<T>() -> impl Layer<T> {
+pub fn new<T>(make_span: &dyn Fn(&Request<Body>) -> Span) -> impl Layer<T> {
     TraceLayer::new_for_http()
         .make_span_with(make_span)
         .on_response(
@@ -24,23 +24,23 @@ pub fn new<T>() -> impl Layer<T> {
         )
 }
 
-fn make_span(request: &Request<Body>) -> tracing::Span {
-    let user_agent = request
-        .headers()
-        .get(header::USER_AGENT)
-        .map(|value| value.to_str().unwrap_or("unknown"));
+// fn make_span(request: &Request<Body>) -> tracing::Span {
+//     let user_agent = request
+//         .headers()
+//         .get(header::USER_AGENT)
+//         .map(|value| value.to_str().unwrap_or("unknown"));
 
-    let app_version = request
-        .headers()
-        .get("X-App-Version")
-        .map(|value| value.to_str().unwrap_or("unknown"));
+//     let app_version = request
+//         .headers()
+//         .get("X-App-Version")
+//         .map(|value| value.to_str().unwrap_or("unknown"));
 
-    tracing::info_span!(
-        "tracing",
-        user_agent,
-        app_version,
-        uri = %request.uri(),
-        solana_addr = tracing::field::Empty,
-        subscriber = tracing::field::Empty,
-    )
-}
+//     tracing::info_span!(
+//         "tracing",
+//         user_agent,
+//         app_version,
+//         uri = %request.uri(),
+//         solana_addr = tracing::field::Empty,
+//         subscriber = tracing::field::Empty,
+//     )
+// }
