c9215e8ea186d24216c70953afbb22ccaf175b62
Macpie <pierre@nova-labs.com>
2024-04-19 10:20:11 -0700

    Fix types (thanks Mr Pickles)

--- custom_tracing/src/grpc_layer.rs
+++ custom_tracing/src/grpc_layer.rs
@@ -1,13 +1,16 @@
 use helium_proto::services::Body;
 use http::request::Request;
 use tower_http::{
+    classify::{GrpcErrorsAsFailures, SharedClassifier},
     trace::{DefaultOnFailure, DefaultOnResponse, TraceLayer},
     LatencyUnit,
 };
-use tower_layer::Layer;
 use tracing::{Level, Span};
 
-pub fn new<T>(make_span: &dyn Fn(&Request<Body>) -> Span) -> impl Layer<T> {
+pub fn new(
+    make_span: fn(&Request<Body>) -> Span,
+) -> TraceLayer<SharedClassifier<GrpcErrorsAsFailures>, for<'a> fn(&'a http::Request<Body>) -> Span>
+{
     TraceLayer::new_for_grpc()
         .make_span_with(make_span)
         .on_response(
@@ -19,5 +22,5 @@ pub fn new<T>(make_span: &dyn Fn(&Request<Body>) -> Span) -> impl Layer<T> {
             DefaultOnFailure::new()
                 .level(Level::WARN)
                 .latency_unit(LatencyUnit::Micros),
-        );
+        )
 }
