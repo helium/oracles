a752017597214a5870d616e9b50ac52f8c8f8643
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-05-20 15:16:20 -0700

    super simple calculate function

--- coverage_point_calculator/src/lib.rs
+++ coverage_point_calculator/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+struct Radio;
+#[derive(Debug)]
+struct BoostedHexes;
+#[derive(Debug)]
+struct CoverageMap;
+
+#[derive(Debug, PartialEq)]
+struct CoveragePoints;
+
+fn calculate(
+    _radio: Radio,
+    _boosted_hexes: BoostedHexes,
+    _coverage_map: CoverageMap,
+) -> CoveragePoints {
+    CoveragePoints
 }
 
 #[cfg(test)]
@@ -8,7 +22,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let radio = Radio;
+        let boosted_hexes = BoostedHexes;
+        let coverage_map = CoverageMap;
+
+        let coverage_points = calculate(radio, boosted_hexes, coverage_map);
+
+        assert_eq!(coverage_points, CoveragePoints);
     }
 }
