ff8d81af3a4ee7b97efd07f2cfce552ca1064244
Macpie <pierre@nova-labs.com>
2024-05-23 13:51:09 -0700

    Fixup seeder and packet verifier startup

--- docker/mobile/postgres_seeder/run.sh
+++ docker/mobile/postgres_seeder/run.sh
@@ -2,19 +2,26 @@
 
 echo "#############################################"
 echo "Running mobile-config migrations"
-sqlx migrate run --database-url $DB_URL/mobile_config --source /mobile_config/migrations
+sqlx migrate run --database-url postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/mobile_config --source /mobile_config/migrations
 echo "#############################################"
 
 echo "Running mobile-packet-verifier migrations"
-sqlx migrate run --database-url $DB_URL/mobile_packet_verifier --source /mobile_packet_verifier/migrations
+sqlx migrate run --database-url postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/mobile_packet_verifier --source /mobile_packet_verifier/migrations
 echo "#############################################"
 
 echo "Running mobile-verifier migrations"
-sqlx migrate run --database-url $DB_URL/mobile_verifier --source /mobile_verifier/migrations
+sqlx migrate run --database-url postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/mobile_verifier --source /mobile_verifier/migrations
 echo "#############################################"
 
 echo "Running mobile reward-index migrations"
-sqlx migrate run --database-url $DB_URL/mobile_index --source /reward_index/migrations
+sqlx migrate run --database-url postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/mobile_index --source /reward_index/migrations
 echo "#############################################"
 
-echo "Done"
+export PGPASSWORD=$POSTGRES_PASSWORD
+MOBILE_CONFIG_FILES=/postgres_seeder/mobile_config/*
+
+for file in $MOBILE_CONFIG_FILES; do
+    echo "Running $file"
+    psql -h $POSTGRES_HOST -U $POSTGRES_USER -d mobile_config -f $file
+    echo "#############################################"
+done
