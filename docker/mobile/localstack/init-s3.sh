dc60ed5d5361d083136bdde97c479dd9c24e0998
Macpie <pierre@nova-labs.com>
2024-05-24 11:07:37 -0700

    Upgrade to latest main and init verifier bucket with local dataset

--- docker/mobile/localstack/init-s3.sh
+++ docker/mobile/localstack/init-s3.sh
@@ -4,3 +4,6 @@ awslocal s3 mb s3://mobile-ingest
 awslocal s3 mb s3://mobile-verifier
 awslocal s3 mb s3://mobile-packet-verifier
 awslocal s3 mb s3://mobile-price
+awslocal s3 mb s3://mobile-verifier-data-sets
+
+awslocal s3 cp /tmp/data/ s3://mobile-verifier-data-sets/ --recursive
