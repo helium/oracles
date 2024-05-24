b97507818a3550c9169e7cbe55519a3d57807744
Macpie <pierre@nova-labs.com>
2024-05-24 15:03:50 -0700

    Auto load data sets for verifier

--- docker/mobile/localstack/init-s3.sh
+++ docker/mobile/localstack/init-s3.sh
@@ -6,4 +6,11 @@ awslocal s3 mb s3://mobile-packet-verifier
 awslocal s3 mb s3://mobile-price
 awslocal s3 mb s3://mobile-verifier-data-sets
 
-awslocal s3 cp /tmp/data/ s3://mobile-verifier-data-sets/ --recursive
+DIRS=/tmp/data/*
+for DIR in $DIRS; do
+    readarray -d "-" -t array <<<"$DIR"
+    bucket=$(echo $DIR | sed 's|/tmp/data/||g')
+    echo "Uploading $DIR to bucket $bucket"
+
+    awslocal s3 cp $DIR s3://$bucket/ --recursive
+done
