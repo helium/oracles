1be6341ba977ad50cd2556d8d1699791bc79dd32
Macpie <pierre@nova-labs.com>
2024-05-30 12:30:31 -0700

    Test skeleton

--- test_mobile/tests/docker/mod.rs
+++ test_mobile/tests/docker/mod.rs
@@ -1,40 +1,57 @@
 use anyhow::{bail, Result};
+use std::process::Command;
 use std::time::Duration;
-use tokio::process::Command;
 use tonic::transport::{Channel, Uri};
 
-pub async fn up() -> Result<String> {
-    let up_output = Command::new("docker")
-        .current_dir("../docker/mobile/")
-        .arg("compose")
-        .arg("up")
-        .arg("-d")
-        .output()
-        .await?;
+pub struct Docker;
 
-    if up_output.status.success() {
-        let stdout = String::from_utf8(up_output.stdout)?;
-        Ok(stdout)
-    } else {
-        let stderr = String::from_utf8(up_output.stderr)?;
-        bail!(stderr)
+impl Docker {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn up(&self) -> Result<String> {
+        let up_output = Command::new("docker")
+            .current_dir("../docker/mobile/")
+            .arg("compose")
+            .arg("up")
+            .arg("-d")
+            .output()?;
+
+        if up_output.status.success() {
+            let stdout = String::from_utf8(up_output.stdout)?;
+            Ok(stdout)
+        } else {
+            let stderr = String::from_utf8(up_output.stderr)?;
+            bail!(stderr)
+        }
+    }
+
+    fn down(&self) -> Result<String> {
+        let up_output = Command::new("docker")
+            .current_dir("../docker/mobile/")
+            .arg("compose")
+            .arg("down")
+            .output()?;
+
+        if up_output.status.success() {
+            let stdout = String::from_utf8(up_output.stdout)?;
+            Ok(stdout)
+        } else {
+            let stderr = String::from_utf8(up_output.stderr)?;
+            bail!(stderr)
+        }
     }
 }
 
-pub async fn down() -> Result<String> {
-    let up_output = Command::new("docker")
-        .current_dir("../docker/mobile/")
-        .arg("compose")
-        .arg("down")
-        .output()
-        .await?;
-
-    if up_output.status.success() {
-        let stdout = String::from_utf8(up_output.stdout)?;
-        Ok(stdout)
-    } else {
-        let stderr = String::from_utf8(up_output.stderr)?;
-        bail!(stderr)
+impl Drop for Docker {
+    fn drop(&mut self) {
+        // This code runs when the scope exits, including if the test fails.
+        tracing::info!("Test finished. Performing cleanup.");
+        match self.down() {
+            Ok(_) => tracing::info!("stack went down"),
+            Err(e) => tracing::error!("docker compose down failed: {:?}", e),
+        }
     }
 }
 
--- test_mobile/tests/integration_test.rs
+++ test_mobile/tests/integration_test.rs
@@ -1,5 +1,15 @@
 use anyhow::Result;
-use std::time::Duration;
+use backon::{ExponentialBuilder, Retryable};
+use docker::Docker;
+use helium_crypto::{KeyTag, Keypair, Sign};
+use helium_proto::services::poc_mobile::{Client as PocMobileClient, SpeedtestReqV1};
+use prost::Message;
+use rand::rngs::OsRng;
+use std::{
+    str::FromStr,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+use tonic::metadata::MetadataValue;
 
 mod docker;
 
@@ -7,7 +17,9 @@ mod docker;
 async fn main() -> Result<()> {
     custom_tracing::init("info".to_string(), custom_tracing::Settings::default()).await?;
 
-    match docker::up().await {
+    let docker = Docker::new();
+
+    match docker.up() {
         Ok(_) => {
             tracing::info!("docker compose started")
         }
@@ -25,7 +37,47 @@ async fn main() -> Result<()> {
         Err(e) => panic!("docker::check_ingest_up failed: {:?}", e),
     }
 
-    docker::down().await?;
+    // let _ = tokio::time::sleep(Duration::from_secs(10)).await;
+
+    let mut client = (|| PocMobileClient::connect(endpoint))
+        .retry(&ExponentialBuilder::default())
+        .await
+        .expect("client connect");
+
+    let hotspot1 = generate_keypair();
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    let mut speedtest_req = SpeedtestReqV1 {
+        pub_key: hotspot1.public_key().into(),
+        serial: "hotspot1-serial".to_string(),
+        timestamp: now as u64,
+        upload_speed: 100,
+        download_speed: 100,
+        latency: 25,
+        signature: vec![],
+    };
+
+    speedtest_req.signature = hotspot1.sign(&speedtest_req.encode_to_vec()).expect("sign");
+
+    let mut request = tonic::Request::new(speedtest_req);
+
+    let metadata_value = MetadataValue::from_str("Bearer api-token").unwrap();
+    request
+        .metadata_mut()
+        .insert("authorization", metadata_value);
+
+    let _r = client.submit_speedtest(request).await?;
+
+    // let _ = tokio::time::sleep(Duration::from_secs(2)).await;
+
+    // assert_eq!(true, false);
 
     Ok(())
+}
+
+fn generate_keypair() -> Keypair {
+    Keypair::generate(KeyTag::default(), &mut OsRng)
 }
