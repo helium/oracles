1d91a07b2d32c40f5848a6e0fa6ec67cb2774e7d
Brian Balser <bbalser@nova-labs.com>
2024-06-26 16:46:22 -0400

    Update mobile ingest to support sp_boosted_rewards_ban_radio messages

--- file_store/src/file_info.rs
+++ file_store/src/file_info.rs
@@ -157,6 +157,7 @@ pub const ORACLE_BOOSTING_REPORT: &str = "oracle_boosting_report";
 pub const URBANIZATION_DATA_SET: &str = "urbanization";
 pub const FOOTFALL_DATA_SET: &str = "footfall";
 pub const LANDTYPE_DATA_SET: &str = "landtype";
+pub const SP_BOOSTED_REWARDS_BANNED_RADIO: &str = "service_provider_boosted_rewards_banned_radio";
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Copy, strum::EnumCount)]
 #[serde(rename_all = "snake_case")]
@@ -209,6 +210,7 @@ pub enum FileType {
     InvalidatedRadioThresholdReq,
     InvalidatedRadioThresholdIngestReport,
     VerifiedInvalidatedRadioThresholdIngestReport,
+    ServiceProviderBoostedRewardsBannedRadioIngestReport,
 }
 
 impl fmt::Display for FileType {
@@ -270,6 +272,9 @@ impl fmt::Display for FileType {
             Self::UrbanizationDataSet => URBANIZATION_DATA_SET,
             Self::FootfallDataSet => FOOTFALL_DATA_SET,
             Self::LandtypeDataSet => LANDTYPE_DATA_SET,
+            Self::ServiceProviderBoostedRewardsBannedRadioIngestReport => {
+                SP_BOOSTED_REWARDS_BANNED_RADIO
+            }
         };
         f.write_str(s)
     }
@@ -334,6 +339,9 @@ impl FileType {
             Self::UrbanizationDataSet => URBANIZATION_DATA_SET,
             Self::FootfallDataSet => FOOTFALL_DATA_SET,
             Self::LandtypeDataSet => LANDTYPE_DATA_SET,
+            Self::ServiceProviderBoostedRewardsBannedRadioIngestReport => {
+                SP_BOOSTED_REWARDS_BANNED_RADIO
+            }
         }
     }
 }
@@ -398,6 +406,9 @@ impl FromStr for FileType {
             URBANIZATION_DATA_SET => Self::UrbanizationDataSet,
             FOOTFALL_DATA_SET => Self::FootfallDataSet,
             LANDTYPE_DATA_SET => Self::LandtypeDataSet,
+            SP_BOOSTED_REWARDS_BANNED_RADIO => {
+                Self::ServiceProviderBoostedRewardsBannedRadioIngestReport
+            }
             _ => return Err(Error::from(io::Error::from(io::ErrorKind::InvalidInput))),
         };
         Ok(result)
