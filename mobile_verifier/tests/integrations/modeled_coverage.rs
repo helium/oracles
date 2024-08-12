024dccba5a81d2e92f025ee76e08017ef8e67782
Brian Balser <brian.balser@gmail.com>
2024-08-12 14:33:16 -0400

    Support new location source field on wifi heartbeats (#833)
    * Support new location source field on wifi heartbeats
    
    * fix imports
    
    * Reset proto back to master

--- mobile_verifier/tests/integrations/modeled_coverage.rs
+++ mobile_verifier/tests/integrations/modeled_coverage.rs
@@ -10,7 +10,7 @@ use h3o::{CellIndex, LatLng};
 use helium_crypto::PublicKeyBinary;
 use helium_proto::services::{
     mobile_config::NetworkKeyRole,
-    poc_mobile::{CoverageObjectValidity, SignalLevel},
+    poc_mobile::{CoverageObjectValidity, LocationSource, SignalLevel},
 };
 use hextree::Cell;
 use mobile_config::boosted_hex_info::{BoostedHexInfo, BoostedHexes};
@@ -1395,6 +1395,7 @@ async fn ensure_lower_trust_score_for_distant_heartbeats(pool: PgPool) -> anyhow
             location_validation_timestamp: Some(DateTime::<Utc>::MIN_UTC),
             operation_mode: true,
             coverage_object: Vec::from(coverage_object_uuid.into_bytes()),
+            location_source: LocationSource::Skyhook,
         },
         received_timestamp: Utc::now(),
     };
