901d6fd5e33645a5da0e4d5b63a8072e120f5c54
macpie <macpie@users.noreply.github.com>
2024-09-04 10:52:07 -0700

    Make sure that default is respected when `[custom_tracing]` is missing (#862)

--- custom_tracing/src/settings.rs
+++ custom_tracing/src/settings.rs
@@ -1,10 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Settings {
     /// File name to be watched by custom tracing
     #[serde(default = "default_tracing_cfg_file")]
     pub tracing_cfg_file: String,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            tracing_cfg_file: default_tracing_cfg_file(),
+        }
+    }
 }
 
 pub fn default_tracing_cfg_file() -> String {
