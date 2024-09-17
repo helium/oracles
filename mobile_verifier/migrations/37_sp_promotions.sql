f1cb9a9c915984074f61013c0e0ffa4193bf644b
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-09-16 17:39:57 -0700

    move promotion fund storage into migration with rewards tracking

--- mobile_verifier/migrations/37_sp_promotions.sql
+++ mobile_verifier/migrations/37_sp_promotions.sql
@@ -13,3 +13,9 @@ CREATE TABLE IF NOT EXISTS gateway_promotion_rewards (
        shares BIGINT NOT NULL,
        PRIMARY KEY (time_of_reward, gateway_key, carrier_key) 
 );
+
+CREATE TABLE IF NOT EXISTS service_provider_promotion_funds (
+       service_provider BIGINT NOT NULL PRIMARY KEY,
+       basis_points BIGINT NOT NULL,
+       inserted_at TIMESTAMPTZ NOT NULL
+);
