5c25c86482172fcbec8b34e2f5ff4a8ab3bbde31
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-10-17 15:12:45 -0700

    Keep importing RadioReward until we stop reading them

--- mobile_verifier/tests/integrations/common/mod.rs
+++ mobile_verifier/tests/integrations/common/mod.rs
@@ -10,7 +10,8 @@ use helium_proto::services::{
     poc_mobile::{
         mobile_reward_share::Reward as MobileReward, radio_reward_v2, GatewayReward,
         MobileRewardShare, OracleBoostingHexAssignment, OracleBoostingReportV1, PromotionReward,
-        RadioRewardV2, ServiceProviderReward, SpeedtestAvg, SubscriberReward, UnallocatedReward,
+        RadioReward, RadioRewardV2, ServiceProviderReward, SpeedtestAvg, SubscriberReward,
+        UnallocatedReward,
     },
 };
 use hex_assignments::{Assignment, HexAssignment, HexBoostData};
