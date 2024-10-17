9ca198c608084b6a6f949a726563a0565ff7be7b
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2024-10-17 10:38:26 +0100

    use updated proto with renamed fields

--- ingest/tests/mobile_ingest.rs
+++ ingest/tests/mobile_ingest.rs
@@ -44,18 +44,16 @@ async fn submit_hex_usage_report() -> anyhow::Result<()> {
     let (mut client, trigger) = common::setup_mobile().await?;
 
     const HEX: u64 = 360;
-    const HELIUM_MOBILE_SUBSCRIBER_AVG_COUNT: u64 = 10;
-    const HELIUM_MOBILE_DISCO_MAPPING_AVG_COUNT: u64 = 11;
+    const SERVICE_PROVIDER_SUBSCRIBER_AVG_COUNT: u64 = 10;
+    const DISCO_MAPPING_AVG_COUNT: u64 = 11;
     const OFFLOAD_AVG_COUNT: u64 = 12;
-    const TMO_CELL_AVG_COUNT: u64 = 13;
 
     let res = client
         .submit_hex_usage_req(
             HEX,
-            HELIUM_MOBILE_SUBSCRIBER_AVG_COUNT,
-            HELIUM_MOBILE_DISCO_MAPPING_AVG_COUNT,
+            SERVICE_PROVIDER_SUBSCRIBER_AVG_COUNT,
+            DISCO_MAPPING_AVG_COUNT,
             OFFLOAD_AVG_COUNT,
-            TMO_CELL_AVG_COUNT,
         )
         .await;
 
@@ -72,15 +70,14 @@ async fn submit_hex_usage_report() -> anyhow::Result<()> {
                 Some(event) => {
                     assert_eq!(HEX, event.hex);
                     assert_eq!(
-                        HELIUM_MOBILE_SUBSCRIBER_AVG_COUNT,
-                        event.helium_mobile_subscriber_avg_count
+                        SERVICE_PROVIDER_SUBSCRIBER_AVG_COUNT,
+                        event.service_provider_subscriber_avg_count
                     );
                     assert_eq!(
-                        HELIUM_MOBILE_DISCO_MAPPING_AVG_COUNT,
-                        event.helium_mobile_disco_mapping_avg_count
+                        DISCO_MAPPING_AVG_COUNT,
+                        event.disco_mapping_avg_count
                     );
                     assert_eq!(OFFLOAD_AVG_COUNT, event.offload_avg_count);
-                    assert_eq!(TMO_CELL_AVG_COUNT, event.tmo_cell_avg_count);
                 }
             }
         }
@@ -97,16 +94,16 @@ async fn submit_radio_usage_report() -> anyhow::Result<()> {
 
     let hotspot_pubkey = PublicKeyBinary::from_str(PUBKEY1)?;
     let cbsd_id = "cbsd_id".to_string();
-    const HELIUM_MOBILE_SUBSCRIBER_AVG_COUNT: u64 = 10;
-    const HELIUM_MOBILE_DISCO_MAPPING_AVG_COUNT: u64 = 11;
+    const SERVICE_PROVIDER_SUBSCRIBER_AVG_COUNT: u64 = 10;
+    const DISCO_MAPPING_AVG_COUNT: u64 = 11;
     const OFFLOAD_AVG_COUNT: u64 = 12;
 
     let res = client
         .submit_radio_usage_req(
             hotspot_pubkey.clone(),
             cbsd_id.clone(),
-            HELIUM_MOBILE_SUBSCRIBER_AVG_COUNT,
-            HELIUM_MOBILE_DISCO_MAPPING_AVG_COUNT,
+            SERVICE_PROVIDER_SUBSCRIBER_AVG_COUNT,
+            DISCO_MAPPING_AVG_COUNT,
             OFFLOAD_AVG_COUNT,
         )
         .await;
@@ -125,13 +122,10 @@ async fn submit_radio_usage_report() -> anyhow::Result<()> {
                     assert_eq!(hotspot_pubkey.as_ref(), event.hotspot_pubkey);
                     assert_eq!(cbsd_id, event.cbsd_id);
                     assert_eq!(
-                        HELIUM_MOBILE_SUBSCRIBER_AVG_COUNT,
-                        event.helium_mobile_subscriber_avg_count
+                        SERVICE_PROVIDER_SUBSCRIBER_AVG_COUNT,
+                        event.service_provider_subscriber_avg_count
                     );
-                    assert_eq!(
-                        HELIUM_MOBILE_DISCO_MAPPING_AVG_COUNT,
-                        event.helium_mobile_disco_mapping_avg_count
-                    );
+                    assert_eq!(DISCO_MAPPING_AVG_COUNT, event.disco_mapping_avg_count);
                     assert_eq!(OFFLOAD_AVG_COUNT, event.offload_avg_count);
                 }
             }
