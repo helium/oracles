9882d6190a91ef3a6d1275f814bc0e39f1f65861
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-10-30 14:09:48 -0700

    Add CLI for fetching sp promotions
    Fetch from mobile-config the same way they as the rewarder does. Print them in a nice little list.

--- mobile_verifier/src/cli/mod.rs
+++ mobile_verifier/src/cli/mod.rs
@@ -1,3 +1,4 @@
 pub mod reward_from_db;
 pub mod server;
+pub mod service_provider_promotions;
 pub mod verify_disktree;
--- mobile_verifier/src/main.rs
+++ mobile_verifier/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use mobile_verifier::{
-    cli::{reward_from_db, server, verify_disktree},
+    cli::{reward_from_db, server, service_provider_promotions, verify_disktree},
     Settings,
 };
 use std::path;
@@ -37,6 +37,8 @@ pub enum Cmd {
     /// Go through every cell and ensure it's value can be turned into an Assignment.
     /// NOTE: This can take a very long time. Run with a --release binary.
     VerifyDisktree(verify_disktree::Cmd),
+    /// Print active Service Provider Promotions
+    ServiceProviderPromotions(service_provider_promotions::Cmd),
 }
 
 impl Cmd {
@@ -45,6 +47,7 @@ impl Cmd {
             Self::Server(cmd) => cmd.run(&settings).await,
             Self::RewardFromDb(cmd) => cmd.run(&settings).await,
             Self::VerifyDisktree(cmd) => cmd.run(&settings).await,
+            Self::ServiceProviderPromotions(cmd) => cmd.run(&settings).await,
         }
     }
 }
