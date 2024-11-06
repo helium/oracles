fba2fda1f2f525a29ad989c8796392c056ab9671
Macpie <pierre@nova-labs.com>
2024-11-06 14:49:23 -0800

    Fix later table and drop olf foreign key

--- mobile_verifier/migrations/38_coverage_objects_cascade_delete.sql
+++ mobile_verifier/migrations/38_coverage_objects_cascade_delete.sql
@@ -1,4 +1,7 @@
 ALTER TABLE
+    hexes DROP CONSTRAINT IF EXISTS hexes_uuid_fkey;
+
+ALTER TABLE
     hexes
 ADD
     CONSTRAINT hexes_uuid_fkey FOREIGN KEY (uuid) REFERENCES coverage_objects(uuid) ON DELETE CASCADE;
