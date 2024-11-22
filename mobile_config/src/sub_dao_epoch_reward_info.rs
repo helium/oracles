a6e2de336864271521f0909bee7d71553344e302
Andrew McKenzie <andrewb.mckenzie@gmail.com>
2024-11-22 15:44:47 +0000

    actually start the subdao service. fix db access

--- mobile_config/src/sub_dao_epoch_reward_info.rs
+++ mobile_config/src/sub_dao_epoch_reward_info.rs
@@ -3,7 +3,6 @@ use chrono::{DateTime, Utc};
 use file_store::traits::{TimestampDecode, TimestampEncode};
 use helium_proto::services::sub_dao::SubDaoEpochRewardInfo as SubDaoEpochRewardInfoProto;
 use rust_decimal::prelude::*;
-use sqlx::FromRow;
 use std::ops::Range;
 
 #[derive(Clone, Debug)]
@@ -16,15 +15,12 @@ pub struct ResolvedSubDaoEpochRewardInfo {
     pub rewards_issued_at: DateTime<Utc>,
 }
 
-#[derive(Clone, Debug, FromRow)]
+#[derive(Clone, Debug)]
 pub struct RawSubDaoEpochRewardInfo {
-    #[sqlx(try_from = "i64")]
     epoch: u64,
     epoch_address: String,
     sub_dao_address: String,
-    #[sqlx(try_from = "i64")]
     rewards_issued: u64,
-    #[sqlx(try_from = "i64")]
     delegation_rewards_issued: u64,
     rewards_issued_at: DateTime<Utc>,
 }
@@ -63,18 +59,19 @@ impl TryFrom<SubDaoEpochRewardInfoProto> for ResolvedSubDaoEpochRewardInfo {
 }
 
 pub(crate) mod db {
-
     use crate::sub_dao_epoch_reward_info::RawSubDaoEpochRewardInfo;
-    use sqlx::PgExecutor;
+    use chrono::{DateTime, Utc};
+    use file_store::traits::TimestampDecode;
+    use sqlx::{postgres::PgRow, FromRow, PgExecutor, Row};
 
     const GET_EPOCH_REWARD_INFO_SQL: &str = r#"
             SELECT
                 address AS epoch_pubkey,
                 sub_dao AS sub_dao_pubkey,
-                epoch,
-                rewards_issued,
-                delegation_rewards_issued,
-                rewards_issued_at
+                epoch::BIGINT,
+                delegation_rewards_issued::BIGINT AS rewards_issued,
+                delegation_rewards_issued::BIGINT,
+                rewards_issued_at::BIGINT
             FROM sub_dao_epoch_infos
             WHERE epoch = $1 AND sub_dao = $2
         "#;
@@ -92,5 +89,23 @@ pub(crate) mod db {
             .bind(sub_dao_address)
             .fetch_optional(db)
             .await?)
+    }
+
+    impl FromRow<'_, PgRow> for RawSubDaoEpochRewardInfo {
+        fn from_row(row: &PgRow) -> sqlx::Result<Self> {
+            let rewards_issued_at: DateTime<Utc> = (row.try_get::<i64, &str>("rewards_issued_at")?
+                as u64)
+                .to_timestamp()
+                .map_err(|err| sqlx::Error::Decode(Box::new(err)))?;
+
+            Ok(Self {
+                epoch: row.get::<i64, &str>("epoch") as u64,
+                epoch_address: row.get::<String, &str>("epoch_address"),
+                sub_dao_address: row.get::<String, &str>("sub_dao_address"),
+                rewards_issued: row.get::<i64, &str>("rewards_issued") as u64,
+                delegation_rewards_issued: row.get::<i64, &str>("delegation_rewards_issued") as u64,
+                rewards_issued_at,
+            })
+        }
     }
 }
