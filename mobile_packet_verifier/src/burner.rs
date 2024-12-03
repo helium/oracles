fe4241d35339a78ea6ab63a9c42a705f037fd6d4
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-12-03 15:46:40 -0700

    Fallback to confirming transactions on all errors
    If a submitted transaction does not come back as a defacto success, we spawn a process that checks a txn made it to the chain by signature. By default, we will retry 5 times a minute apart, before considering the txn an actual failure to be tried again later.
    
    If during the checking we find the txn on chain, we do the same logic as if it had passed the first time.

--- mobile_packet_verifier/src/burner.rs
+++ mobile_packet_verifier/src/burner.rs
@@ -45,43 +45,138 @@ where
             let payer_balance = self.solana.payer_balance(&payer).await?;
 
             if payer_balance < total_dcs {
-                tracing::warn!(%payer, %payer_balance, %total_dcs, "Payer does not have enough balance to burn dcs");
+                tracing::warn!(
+                    %payer,
+                    %payer_balance,
+                    %total_dcs,
+                    "Payer does not have enough balance to burn dcs"
+                );
                 continue;
             }
 
             tracing::info!(%total_dcs, %payer, "Burning DC");
-            if self.burn_data_credits(&payer, total_dcs).await.is_err() {
-                // We have failed to burn data credits:
-                metrics::counter!("burned", "payer" => payer.to_string(), "success" => "false")
-                    .increment(total_dcs);
-                continue;
-            }
+            let txn = self.solana.make_burn_transaction(&payer, total_dcs).await?;
+            match self.solana.submit_transaction(&txn).await {
+                Err(err) => {
+                    let span = tracing::info_span!(
+                        "transaction_confirmation",
+                        signature = %txn.get_signature(),
+                        %payer,
+                        total_dcs,
+                        max_attempts = self.retry_attempts
+                    );
 
-            // We succesfully managed to burn data credits:
+                    tokio::spawn(
+                        self.transaction_confirmation_check(
+                            pool, err, txn, payer, total_dcs, sessions,
+                        )
+                        .instrument(span),
+                    );
 
-            metrics::counter!("burned", "payer" => payer.to_string(), "success" => "true")
-                .increment(total_dcs);
-
-            // Delete from the data transfer session and write out to S3
-            pending_burns::delete_for_payer(pool, &payer, total_dcs).await?;
-
-            for session in sessions {
-                self.valid_sessions
-                    .write(ValidDataTransferSession::from(session), &[])
+                    continue;
+                }
+                Ok(()) => {
+                    handle_transaction_success(
+                        pool,
+                        payer,
+                        total_dcs,
+                        sessions,
+                        &self.valid_sessions,
+                    )
                     .await?;
+                }
             }
         }
 
         Ok(())
     }
 
-    async fn burn_data_credits(
+    fn transaction_confirmation_check(
         &self,
-        payer: &PublicKeyBinary,
-        amount: u64,
-    ) -> Result<(), S::Error> {
-        let txn = self.solana.make_burn_transaction(payer, amount).await?;
-        self.solana.submit_transaction(&txn).await?;
-        Ok(())
+        pool: &Pool<Postgres>,
+        err: S::Error,
+        txn: S::Transaction,
+        payer: PublicKeyBinary,
+        total_dcs: u64,
+        sessions: Vec<pending_burns::DataTransferSession>,
+    ) -> impl Future<Output = ()> {
+        let pool = pool.clone();
+        let solana = self.solana.clone();
+        let valid_sessions = self.valid_sessions.clone();
+        let retry_attempts = self.retry_attempts;
+        let check_interval = self.failed_check_interval;
+
+        async move {
+            tracing::warn!(?err, "starting txn confirmation check");
+            // We don't know if the transaction actually made it, maybe it did
+
+            let signature = txn.get_signature();
+            for check_idx in 0..retry_attempts {
+                tokio::time::sleep(check_interval).await;
+                match solana.confirm_transaction(signature).await {
+                    Ok(true) => {
+                        tracing::debug!("txn confirmed on chain");
+                        let txn_success = handle_transaction_success(
+                            &pool,
+                            payer,
+                            total_dcs,
+                            sessions,
+                            &valid_sessions,
+                        )
+                        .await;
+                        if let Err(err) = txn_success {
+                            tracing::error!(?err, "txn succeeded, something else failed");
+                        }
+
+                        return;
+                    }
+                    Ok(false) => {
+                        // we did bad, maybe check again?
+                        tracing::info!(check_idx, "transaction not confirmed, yet...");
+                        continue;
+                    }
+                    Err(err) => {
+                        // could not check, maybe check again?
+                        tracing::error!(?err, check_idx, "failed to confirm transaction");
+                        continue;
+                    }
+                }
+            }
+
+            // We have failed to burn data credits:
+            metrics::counter!(
+                "burned",
+                "payer" => payer.to_string(),
+                "success" => "false"
+            )
+            .increment(total_dcs);
+        }
     }
+}
+
+async fn handle_transaction_success(
+    pool: &Pool<Postgres>,
+    payer: PublicKeyBinary,
+    total_dcs: u64,
+    sessions: Vec<pending_burns::DataTransferSession>,
+    valid_sessions: &FileSinkClient<ValidDataTransferSession>,
+) -> Result<(), anyhow::Error> {
+    // We succesfully managed to burn data credits:
+    metrics::counter!(
+        "burned",
+        "payer" => payer.to_string(),
+        "success" => "true"
+    )
+    .increment(total_dcs);
+
+    // Delete from the data transfer session and write out to S3
+    pending_burns::delete_for_payer(pool, &payer, total_dcs).await?;
+
+    for session in sessions {
+        valid_sessions
+            .write(ValidDataTransferSession::from(session), &[])
+            .await?;
+    }
+
+    Ok(())
 }
