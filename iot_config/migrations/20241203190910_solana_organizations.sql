73b6212c631a383533ce7370dd37006f63034e66
bry <bryanzettler@gmail.com>
2024-12-03 14:33:37 -0600

    add triggers to solana_organization table

--- iot_config/migrations/20241203190910_solana_organizations.sql
+++ iot_config/migrations/20241203190910_solana_organizations.sql
@@ -9,3 +9,31 @@ CREATE TABLE IF NOT EXISTS solana_organizations (
     escrow_key TEXT NOT NULL,
     approved BOOLEAN NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION delete_routes_on_solana_organizations_delete() RETURNS trigger AS $$
+BEGIN
+    DELETE FROM routes WHERE routes.oui = OLD.oui;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_routes_on_solana_organizations_delete
+AFTER DELETE ON solana_organizations
+FOR EACH ROW
+EXECUTE FUNCTION delete_routes_on_solana_organizations_delete();
+
+CREATE OR REPLACE FUNCTION add_lock_record_on_solana_organizations_insert() RETURNS trigger AS $$
+BEGIN
+    INSERT INTO organization_locks (organization, locked)
+    SELECT sol_org.address, org.locked
+    FROM solana_organizations sol_org
+    LEFT JOIN organizations org ON sol_org.oui = org.oui
+    WHERE sol_org.address = NEW.address;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER add_lock_record_on_solana_organizations_insert
+AFTER INSERT ON solana_organizations
+FOR EACH ROW
+EXECUTE FUNCTION add_lock_record_on_solana_organizations_insert();
