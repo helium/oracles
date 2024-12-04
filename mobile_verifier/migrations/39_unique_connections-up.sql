c03a4b0fed3b3cf904b8c747de7da093074f184a
Michael Jeffrey <michaeldjeffrey@gmail.com>
2024-12-04 15:29:15 -0700

    Add primary key for hotspot_pubkey and received_timestamp
    this makes sure we're idempotent when reprocessing reports within the same day.

--- mobile_verifier/migrations/39_unique_connections-up.sql
+++ mobile_verifier/migrations/39_unique_connections-up.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS unique_connections (
     unique_connections  BIGINT          NOT NULL,
     start_timestamp     TIMESTAMPTZ     NOT NULL,
     end_timestamp       TIMESTAMPTZ     NOT NULL,
-    received_timestamp  TIMESTAMPTZ     NOT NULL
+    received_timestamp  TIMESTAMPTZ     NOT NULL,
+    primary key(hotspot_pubkey, received_timestamp)
 );
+
