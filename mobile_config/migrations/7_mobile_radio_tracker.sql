2bf486f6481c283efcc9c73e845c96441ff0dfc4
Anatolii Kurotych <akurotych@gmail.com>
2025-02-03 14:16:35 +0200

    Add migration

--- mobile_config/migrations/7_mobile_radio_tracker.sql
+++ mobile_config/migrations/7_mobile_radio_tracker.sql
@@ -3,7 +3,5 @@ CREATE TABLE IF NOT EXISTS mobile_radio_tracker (
     hash TEXT NOT NULL,
     last_changed_at TIMESTAMPTZ NOT NULL,
     last_checked_at TIMESTAMPTZ NOT NULL,
-    asserted_location NUMERIC,
-    asserted_location_changed_at TIMESTAMPTZ,
     PRIMARY KEY (entity_key)
 );
