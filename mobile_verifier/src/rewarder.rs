d3ed29108e4930d6d152ad0d7d50db27897c6986
Macpie <pierre@nova-labs.com>
2025-02-20 10:54:52 -0800

    Fix for Rust 1.85.0

--- mobile_verifier/src/rewarder.rs
+++ mobile_verifier/src/rewarder.rs
@@ -394,7 +394,7 @@ where
         Box::pin(
             handle
                 .map_err(anyhow::Error::from)
-                .and_then(|result| async move { result.map_err(anyhow::Error::from) }),
+                .and_then(|result| async move { result }),
         )
     }
 }
--- mobile_verifier/src/sp_boosted_rewards_bans.rs
+++ mobile_verifier/src/sp_boosted_rewards_bans.rs
@@ -140,7 +140,7 @@ where
         Box::pin(
             handle
                 .map_err(anyhow::Error::from)
-                .and_then(|result| async move { result.map_err(anyhow::Error::from) }),
+                .and_then(|result| async move { result }),
         )
     }
 }
