a708344bacbf350b42bd750a64284d86e8400130
Anatolii Kurotych <akurotych@gmail.com>
2025-03-12 15:16:17 +0200

    Remove test_save_cbrs_coverage_object (there is test_save_wifi_coverage_object)

--- mobile_verifier/tests/integrations/modeled_coverage.rs
+++ mobile_verifier/tests/integrations/modeled_coverage.rs
@@ -118,69 +118,6 @@ async fn test_save_wifi_coverage_object(pool: PgPool) -> anyhow::Result<()> {
 }
 
 #[sqlx::test]
-async fn test_save_cbrs_coverage_object(pool: PgPool) -> anyhow::Result<()> {
-    let cache = CoverageObjectCache::new(&pool);
-    let uuid = Uuid::new_v4();
-    let coverage_claim_time = "2023-08-23 00:00:00.000000000 UTC".parse().unwrap();
-    let key = "P27-SCE4255W120200039521XGB0103";
-    let key = KeyType::from(key);
-
-    assert!(cache.fetch_coverage_object(&uuid, key).await?.is_none());
-
-    let co = file_store::coverage::CoverageObject {
-        pub_key: PublicKeyBinary::from(vec![1]),
-        uuid,
-        key_type: file_store::coverage::KeyType::CbsdId(
-            "P27-SCE4255W120200039521XGB0103".to_string(),
-        ),
-        coverage_claim_time,
-        coverage: vec![
-            file_store::coverage::RadioHexSignalLevel {
-                location: "8a1fb46622dffff".parse().unwrap(),
-                signal_level: SignalLevel::High,
-                signal_power: 1000,
-            },
-            file_store::coverage::RadioHexSignalLevel {
-                location: "8a1fb46632dffff".parse().unwrap(),
-                signal_level: SignalLevel::High,
-                signal_power: 1000,
-            },
-            file_store::coverage::RadioHexSignalLevel {
-                location: "8a1fb46642dffff".parse().unwrap(),
-                signal_level: SignalLevel::High,
-                signal_power: 1000,
-            },
-        ],
-        indoor: true,
-        trust_score: 1000,
-        signature: Vec::new(),
-    };
-    let co = CoverageObject {
-        coverage_object: co,
-        validity: CoverageObjectValidity::Valid,
-    };
-
-    let mut transaction = pool.begin().await?;
-    co.save(&mut transaction).await?;
-
-    // Test coverage claim time
-    let cctc = CoverageClaimTimeCache::new();
-    let expected_coverage_claim_time = cctc
-        .fetch_coverage_claim_time(key, &Some(uuid), &mut transaction)
-        .await?
-        .unwrap();
-
-    assert_eq!(expected_coverage_claim_time, coverage_claim_time);
-
-    transaction.commit().await?;
-
-    let coverage = cache.fetch_coverage_object(&uuid, key).await?.unwrap();
-    assert_eq!(coverage.covered_hexes.len(), 3);
-
-    Ok(())
-}
-
-#[sqlx::test]
 async fn test_coverage_object_save_updates(pool: PgPool) -> anyhow::Result<()> {
     let cache = CoverageObjectCache::new(&pool);
     let uuid = Uuid::new_v4();
