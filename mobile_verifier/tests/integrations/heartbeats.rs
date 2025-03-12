2672736d2b73e71efecd768d237927b8f2933471
Anatolii Kurotych <akurotych@gmail.com>
2025-03-12 13:46:57 +0200

    Rework only_fetch_latest_hotspot to use wifi_heartbeats table

--- mobile_verifier/tests/integrations/heartbeats.rs
+++ mobile_verifier/tests/integrations/heartbeats.rs
@@ -89,44 +89,42 @@ async fn test_save_cbrs_heartbeat(pool: PgPool) -> anyhow::Result<()> {
 
 #[sqlx::test]
 async fn only_fetch_latest_hotspot(pool: PgPool) -> anyhow::Result<()> {
-    let cbsd_id = "P27-SCE4255W120200039521XGB0103".to_string();
     let coverage_object = Uuid::new_v4();
-    let cell_type = CellType::from_cbsd_id(&cbsd_id).expect("unable to get cell_type");
+    let cell_type = CellType::NovaGenericWifiIndoor;
     let hotspot_1: PublicKeyBinary =
         "112NqN2WWMwtK29PMzRby62fDydBJfsCLkCAf392stdok48ovNT6".parse()?;
     let hotspot_2: PublicKeyBinary =
         "11sctWiP9r5wDJVuDe1Th4XSL2vaawaLLSQF8f8iokAoMAJHxqp".parse()?;
     sqlx::query(
         r#"
-INSERT INTO cbrs_heartbeats (cbsd_id, hotspot_key, cell_type, latest_timestamp, truncated_timestamp, coverage_object, location_trust_score_multiplier)
+INSERT INTO wifi_heartbeats (hotspot_key, cell_type, latest_timestamp, truncated_timestamp, coverage_object, location_trust_score_multiplier, distance_to_asserted)
 VALUES
-    ($1, $2, 'sercommindoor', '2023-08-25 00:00:00+00', '2023-08-25 00:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 01:00:00+00', '2023-08-25 01:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 02:00:00+00', '2023-08-25 02:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 03:00:00+00', '2023-08-25 03:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 04:00:00+00', '2023-08-25 04:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 05:00:00+00', '2023-08-25 05:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 06:00:00+00', '2023-08-25 06:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 07:00:00+00', '2023-08-25 07:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 08:00:00+00', '2023-08-25 08:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 09:00:00+00', '2023-08-25 09:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 10:00:00+00', '2023-08-25 10:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 11:00:00+00', '2023-08-25 11:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 12:00:00+00', '2023-08-25 12:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 13:00:00+00', '2023-08-25 13:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 14:00:00+00', '2023-08-25 14:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 15:00:00+00', '2023-08-25 15:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 16:00:00+00', '2023-08-25 16:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 17:00:00+00', '2023-08-25 17:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 18:00:00+00', '2023-08-25 18:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 19:00:00+00', '2023-08-25 19:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 20:00:00+00', '2023-08-25 20:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 21:00:00+00', '2023-08-25 21:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 22:00:00+00', '2023-08-25 22:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 23:00:00+00', '2023-08-25 23:00:00+00', $4, 1.0)
+    ($2, 'novagenericwifiindoor', '2023-08-25 00:00:00+00', '2023-08-25 00:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 01:00:00+00', '2023-08-25 01:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 02:00:00+00', '2023-08-25 02:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 03:00:00+00', '2023-08-25 03:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 04:00:00+00', '2023-08-25 04:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 05:00:00+00', '2023-08-25 05:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 06:00:00+00', '2023-08-25 06:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 07:00:00+00', '2023-08-25 07:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 08:00:00+00', '2023-08-25 08:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 09:00:00+00', '2023-08-25 09:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 10:00:00+00', '2023-08-25 10:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 11:00:00+00', '2023-08-25 11:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 12:00:00+00', '2023-08-25 12:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 13:00:00+00', '2023-08-25 13:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 14:00:00+00', '2023-08-25 14:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 15:00:00+00', '2023-08-25 15:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 16:00:00+00', '2023-08-25 16:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 17:00:00+00', '2023-08-25 17:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 18:00:00+00', '2023-08-25 18:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 19:00:00+00', '2023-08-25 19:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 20:00:00+00', '2023-08-25 20:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 21:00:00+00', '2023-08-25 21:00:00+00', $3, 1.0, 0),
+    ($1, 'novagenericwifiindoor', '2023-08-25 22:00:00+00', '2023-08-25 22:00:00+00', $3, 1.0, 0),
+    ($2, 'novagenericwifiindoor', '2023-08-25 23:00:00+00', '2023-08-25 23:00:00+00', $3, 1.0, 0)
 "#,
     )
-    .bind(&cbsd_id)
     .bind(&hotspot_1)
     .bind(&hotspot_2)
     .bind(coverage_object)
@@ -144,9 +142,9 @@ VALUES
         vec![HeartbeatReward {
             hotspot_key: hotspot_2,
             cell_type,
-            cbsd_id: Some(cbsd_id),
-            trust_score_multipliers: vec![Decimal::ONE; 24],
-            distances_to_asserted: None,
+            cbsd_id: None,
+            trust_score_multipliers: vec![Decimal::ONE; 13],
+            distances_to_asserted: Some(vec![0; 13]),
             coverage_object,
         }]
     );
