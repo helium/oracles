8b28e43ce9c22d0d824180e0d7fdd819c5d48c4e
Anatolii Kurotych <akurotych@gmail.com>
2025-03-12 13:53:39 +0200

    Remove ensure_hotspot_does_not_affect_count cbrs test. (looks like cbrs logic specific)

--- mobile_verifier/tests/integrations/heartbeats.rs
+++ mobile_verifier/tests/integrations/heartbeats.rs
@@ -153,61 +153,6 @@ VALUES
 }
 
 #[sqlx::test]
-async fn ensure_hotspot_does_not_affect_count(pool: PgPool) -> anyhow::Result<()> {
-    let cbsd_id = "P27-SCE4255W120200039521XGB0103".to_string();
-    let coverage_object = Uuid::new_v4();
-    let cell_type = CellType::from_cbsd_id(&cbsd_id).expect("unable to get cell_type");
-    let hotspot_1: PublicKeyBinary =
-        "112NqN2WWMwtK29PMzRby62fDydBJfsCLkCAf392stdok48ovNT6".parse()?;
-    let hotspot_2: PublicKeyBinary =
-        "11sctWiP9r5wDJVuDe1Th4XSL2vaawaLLSQF8f8iokAoMAJHxqp".parse()?;
-    sqlx::query(
-        r#"
-INSERT INTO cbrs_heartbeats (cbsd_id, hotspot_key, cell_type, latest_timestamp, truncated_timestamp, coverage_object, location_trust_score_multiplier)
-VALUES
-    ($1, $2, 'sercommindoor', '2023-08-25 00:00:00+00', '2023-08-25 00:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 01:00:00+00', '2023-08-25 01:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 02:00:00+00', '2023-08-25 02:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 03:00:00+00', '2023-08-25 03:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 04:00:00+00', '2023-08-25 04:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 05:00:00+00', '2023-08-25 05:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 06:00:00+00', '2023-08-25 06:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 07:00:00+00', '2023-08-25 07:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 08:00:00+00', '2023-08-25 08:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 09:00:00+00', '2023-08-25 09:00:00+00', $4, 1.0),
-    ($1, $2, 'sercommindoor', '2023-08-25 10:00:00+00', '2023-08-25 10:00:00+00', $4, 1.0),
-    ($1, $3, 'sercommindoor', '2023-08-25 11:00:00+00', '2023-08-25 11:00:00+00', $4, 1.0)
-"#,
-    )
-    .bind(&cbsd_id)
-    .bind(&hotspot_1)
-    .bind(&hotspot_2)
-    .bind(coverage_object)
-    .execute(&pool)
-    .await?;
-
-    let start_period: DateTime<Utc> = "2023-08-25 00:00:00.000000000 UTC".parse()?;
-    let end_period: DateTime<Utc> = "2023-08-26 00:00:00.000000000 UTC".parse()?;
-    let heartbeat_reward: Vec<_> = HeartbeatReward::validated(&pool, &(start_period..end_period))
-        .try_collect()
-        .await?;
-
-    assert_eq!(
-        heartbeat_reward,
-        vec![HeartbeatReward {
-            hotspot_key: hotspot_2,
-            cell_type,
-            cbsd_id: Some(cbsd_id),
-            trust_score_multipliers: vec![Decimal::ONE; 12],
-            distances_to_asserted: None,
-            coverage_object,
-        }]
-    );
-
-    Ok(())
-}
-
-#[sqlx::test]
 async fn ensure_minimum_count(pool: PgPool) -> anyhow::Result<()> {
     let cbsd_id = "P27-SCE4255W120200039521XGB0103".to_string();
     let coverage_object = Uuid::new_v4();
