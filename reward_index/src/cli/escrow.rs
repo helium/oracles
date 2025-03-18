a4370f10f6c8c77e737815c9ea36834d53c6451c
Michael Jeffrey <michaeldjeffrey@gmail.com>
2025-03-18 16:34:58 -0700

    Keep following the pattern

--- reward_index/src/cli/escrow.rs
+++ reward_index/src/cli/escrow.rs
@@ -1,3 +1,5 @@
+use sqlx::PgPool;
+
 use crate::{db, settings::Mode, Settings};
 
 #[derive(Debug, clap::Args)]
@@ -15,39 +17,20 @@ impl Escrow {
 #[derive(Debug, clap::Subcommand)]
 pub enum EscrowCmds {
     /// Take all known radios in the rewards table and give them an escrow duration of 0 (zero) days.
-    Migrate {
-        /// Date on which to expire the grandfathered escrow_duration of 0 days.
-        #[arg(long)]
-        expires_on: chrono::NaiveDate,
-    },
+    Migrate(EscrowMigrate),
     /// Get the current escrow duration for an address
-    Get {
-        #[arg(short, long)]
-        address: String,
-    },
+    Get(EscrowGet),
     /// Set the escrow duration for an address
-    Set {
-        #[arg(long)]
-        address: String,
-
-        #[arg(long)]
-        days: u32,
-
-        #[arg(long)]
-        expires_on: Option<chrono::NaiveDate>,
-    },
+    Set(EscrowSet),
     /// Delete the escrow duration for an address
-    Delete {
-        #[arg(long)]
-        address: String,
-    },
+    Delete(EscrowDelete),
 }
 
 impl EscrowCmds {
     pub async fn run(self, settings: &Settings) -> anyhow::Result<()> {
         anyhow::ensure!(
             matches!(settings.mode, Mode::Mobile | Mode::MobileEscrowed),
-            "migration is only available for 'mobile' and 'mobile_escrowed'"
+            "escrow commands are only available for 'mobile' and 'mobile_escrowed'"
         );
 
         let app_name = format!("{}_{}", settings.mode, env!("CARGO_PKG_NAME"));
@@ -55,53 +38,108 @@ impl EscrowCmds {
         sqlx::migrate!().run(&pool).await?;
 
         match self {
-            EscrowCmds::Migrate { expires_on } => {
-                let migrated_addresses =
-                    db::escrow_duration::migrate_known_radios(&pool, expires_on).await?;
-                tracing::info!(migrated_addresses, "done");
-            }
-            EscrowCmds::Get { address } => {
-                let duration = db::escrow_duration::get(&pool, &address).await?;
-                match duration {
-                    Some((days, Some(expiration))) => {
-                        println!("{address} has an escrow period of {days} days, expring on {expiration}");
-                    }
-                    Some((days, None)) => {
-                        println!("{address} has an escrow period of {days} days, forever");
-                    }
-                    None => {
-                        println!(
-                            "{address} uses the default escrow duration: {} days",
-                            settings.escrow.default_days
-                        );
-                    }
-                }
-            }
-            EscrowCmds::Set {
-                address,
-                days,
-                expires_on,
-            } => {
-                let _inserted =
-                    db::escrow_duration::insert(&pool, &address, days, expires_on).await?;
+            EscrowCmds::Migrate(cmd) => cmd.run(pool).await?,
+            EscrowCmds::Get(cmd) => cmd.run(pool, settings.escrow.default_days).await?,
+            EscrowCmds::Set(cmd) => cmd.run(pool).await?,
+            EscrowCmds::Delete(cmd) => cmd.run(pool, settings.escrow.default_days).await?,
+        }
 
-                match expires_on {
-                    Some(expiration) => {
-                        println!("{address} now has escrow period of {days} days, expiring on {expiration}");
-                    }
-                    None => {
-                        println!("{address} now has escrow period of {days} days, forever");
-                    }
-                }
+        Ok(())
+    }
+}
+
+#[derive(Debug, clap::Args)]
+pub struct EscrowMigrate {
+    /// Date on which to expire the grandfathered escrow_duration of 0 days.
+    #[arg(long)]
+    pub expires_on: chrono::NaiveDate,
+}
+
+impl EscrowMigrate {
+    async fn run(self, pool: PgPool) -> anyhow::Result<()> {
+        let EscrowMigrate { expires_on } = self;
+
+        let migrated_addresses =
+            db::escrow_duration::migrate_known_radios(&pool, expires_on).await?;
+        println!("Migrated {migrated_addresses} radios");
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, clap::Args)]
+pub struct EscrowGet {
+    #[arg(short, long)]
+    pub address: String,
+}
+
+impl EscrowGet {
+    async fn run(self, pool: PgPool, default_days: u32) -> anyhow::Result<()> {
+        let EscrowGet { address } = self;
+
+        let duration = db::escrow_duration::get(&pool, &address).await?;
+
+        match duration {
+            Some((days, Some(expiration))) => {
+                println!("{address} escrow period is {days} days, expring on {expiration}");
             }
-            EscrowCmds::Delete { address } => {
-                db::escrow_duration::delete(&pool, &address).await?;
-                println!(
-                    "{address} is now using the default escrow duration: {} days",
-                    settings.escrow.default_days
-                )
+            Some((days, None)) => {
+                println!("{address} escrow period is {days} days, forever");
+            }
+            None => {
+                println!("{address} uses the default escrow period: {default_days} days",);
             }
         }
+        Ok(())
+    }
+}
+
+#[derive(Debug, clap::Args)]
+pub struct EscrowSet {
+    #[arg(long)]
+    pub address: String,
+
+    #[arg(long)]
+    pub days: u32,
+
+    #[arg(long)]
+    pub expires_on: Option<chrono::NaiveDate>,
+}
+
+impl EscrowSet {
+    async fn run(self, pool: PgPool) -> anyhow::Result<()> {
+        let EscrowSet {
+            address,
+            days,
+            expires_on,
+        } = self;
+
+        let _inserted = db::escrow_duration::insert(&pool, &address, days, expires_on).await?;
+
+        match expires_on {
+            Some(expiration) => {
+                println!("{address} escrow period is {days} days, expiring on {expiration}");
+            }
+            None => {
+                println!("{address} escrow period is {days} days, forever");
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, clap::Args)]
+pub struct EscrowDelete {
+    #[arg(long)]
+    pub address: String,
+}
+
+impl EscrowDelete {
+    async fn run(self, pool: PgPool, default_days: u32) -> anyhow::Result<()> {
+        let EscrowDelete { address } = self;
+
+        db::escrow_duration::delete(&pool, &address).await?;
+        println!("{address} now uses the default escrow duration: {default_days} days");
 
         Ok(())
     }
