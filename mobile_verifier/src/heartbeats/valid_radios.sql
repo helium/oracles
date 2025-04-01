9b3ee25f37ed780e7455d368f683750f50c631a9
Anatolii Kurotych <akurotych@gmail.com>
2025-04-01 13:36:27 +0300

    Remove cbrs from valid_radios.sql

--- mobile_verifier/src/heartbeats/valid_radios.sql
+++ mobile_verifier/src/heartbeats/valid_radios.sql
@@ -1,39 +1,4 @@
-WITH latest_cbrs_hotspot AS (
-    SELECT DISTINCT ON (cbsd_id)
-        cbsd_id,
-        hotspot_key
-    FROM
-        cbrs_heartbeats
-    WHERE
-        truncated_timestamp >= $1
-        AND truncated_timestamp < $2
-    ORDER BY
-        cbsd_id,
-        latest_timestamp DESC
-),
-heartbeats AS (
-    SELECT
-        lch.hotspot_key,
-        ch.cbsd_id,
-        ch.cell_type,
-        CASE WHEN count(*) >= $3 THEN
-            1.0
-        ELSE
-            0.0
-        END AS heartbeat_multiplier,
-	NULL as distances_to_asserted,
-	ARRAY_AGG(ch.location_trust_score_multiplier) as trust_score_multipliers
-    FROM
-        cbrs_heartbeats ch
-        INNER JOIN latest_cbrs_hotspot lch ON ch.cbsd_id = lch.cbsd_id
-    WHERE
-        ch.truncated_timestamp >= $1
-        AND ch.truncated_timestamp < $2
-    GROUP BY
-        ch.cbsd_id,
-        lch.hotspot_key,
-        ch.cell_type
-    UNION
+WITH heartbeats AS (
     SELECT
         hotspot_key,
         NULL AS cbsd_id,
@@ -54,32 +19,19 @@ heartbeats AS (
         hotspot_key,
         cell_type
 ),
-latest_uuids AS (( SELECT DISTINCT ON (hotspot_key,
-            cbsd_id)
-            hotspot_key,
-            cbsd_id,
-            coverage_object
-        FROM
-            cbrs_heartbeats ch
-        WHERE
-            truncated_timestamp >= $1
-            AND truncated_timestamp < $2
-        ORDER BY
-            hotspot_key,
-            cbsd_id,
-            truncated_timestamp DESC)
-    UNION ( SELECT DISTINCT ON (hotspot_key)
-            hotspot_key,
-            NULL AS cbsd_id,
-            coverage_object
-        FROM
-            wifi_heartbeats wh
-        WHERE
-            truncated_timestamp >= $1
-            AND truncated_timestamp < $2
-        ORDER BY
-            hotspot_key,
-            truncated_timestamp DESC))
+latest_uuids AS (
+    SELECT DISTINCT ON (hotspot_key)
+        hotspot_key,
+        coverage_object
+    FROM
+        wifi_heartbeats wh
+    WHERE
+        truncated_timestamp >= $1
+        AND truncated_timestamp < $2
+    ORDER BY
+        hotspot_key,
+        truncated_timestamp DESC
+)
 SELECT
     hb.hotspot_key,
     hb.cell_type,
@@ -89,8 +41,5 @@ SELECT
 FROM
     heartbeats hb
     INNER JOIN latest_uuids u ON hb.hotspot_key = u.hotspot_key
-        AND (hb.cbsd_id = u.cbsd_id
-            OR (hb.cbsd_id IS NULL
-                AND u.cbsd_id IS NULL))
 WHERE
     hb.heartbeat_multiplier = 1.0
