d2998818a4277ca7e526f5b4a6b1d428d92837d0
Anatolii Kurotych <akurotych@gmail.com>
2025-07-11 17:54:45 +0300

    Add PostMigrateMobileTracker cmd

--- mobile_config/src/mobile_radio_tracker.rs
+++ mobile_config/src/mobile_radio_tracker.rs
@@ -1,7 +1,6 @@
 use std::{collections::HashMap, time::Duration};
 
 use chrono::{DateTime, Utc};
-use csv::Reader;
 use futures::{Stream, StreamExt, TryFutureExt, TryStreamExt};
 use helium_crypto::PublicKeyBinary;
 use sqlx::{Pool, Postgres, QueryBuilder};
@@ -298,125 +297,55 @@ async fn update_tracked_radios(
     Ok(())
 }
 
-// This function can be removed after migration is done.
-// Expected CSV example:
-//    "public_key","time","h3"
-//    "1trSus...srQqM1P",2024-09-20 15:12:55.000 +0000,"8c2a10705a4cbff"
-// 1. Fill mobile_radio_tracker asserted_location from mobile_hotspot_infos
-// 2. Read data from csv report. Fill mobile_radio_tracker.asserted_location_changed_at if location from csv and in mobile_hotspot_infos table matches
-// 3. Set `asserted_location_changed_at = created_at` for others (num_location_asserts > 0)
-pub async fn migrate_mobile_tracker_locations(
+// 1. Fill missed `asserted_location_changed_at` in mobile_radio_tracker table
+pub async fn post_migrate_mobile_tracker_locations(
     mobile_config_pool: Pool<Postgres>,
     metadata_pool: Pool<Postgres>,
-    csv_file_path: &str,
 ) -> anyhow::Result<()> {
-    // 1. Fill mobile_radio_tracker asserted_location from mobile_hotspot_infos
+    let mut txn = mobile_config_pool.begin().await?;
 
-    // get_all_mobile_radios
-    tracing::info!("Exporting data from mobile_hotspot_infos");
     let mobile_infos = get_all_mobile_radios(&metadata_pool)
         .filter(|v| futures::future::ready(v.location.is_some()))
-        .filter(|v| futures::future::ready(v.num_location_asserts.is_some_and(|num| num > 0)))
+        .filter(|v| futures::future::ready(v.num_location_asserts.is_some_and(|num| num >= 1)))
         .collect::<Vec<_>>()
         .await;
 
-    let mut txn = mobile_config_pool.begin().await?;
+    // Get inconsistent mobile_radio_tracker rows
+    let mut tracked_radios = get_tracked_radios(&mobile_config_pool).await?;
+    tracked_radios
+        .retain(|_k, v| v.asserted_location.is_some() && v.asserted_location_changed_at.is_none());
+    tracing::info!(
+        "Count of tracked radios with inconsistent info before migration: {}",
+        tracked_radios.len()
+    );
 
-    const BATCH_SIZE: usize = (u16::MAX / 3) as usize;
-
-    // Set asserted_location in mobile_radio_tracker from metadata_pool
-    for chunk in mobile_infos.chunks(BATCH_SIZE) {
-        let mut query_builder = QueryBuilder::new(
-            "UPDATE mobile_radio_tracker AS mrt SET asserted_location = data.location
-         FROM ( ",
-        );
-
-        query_builder.push_values(chunk, |mut builder, mob_info| {
-            builder
-                .push_bind(mob_info.location)
-                .push_bind(&mob_info.entity_key);
-        });
-
-        query_builder.push(
-            ") AS data(location, entity_key)
-         WHERE mrt.entity_key = data.entity_key",
-        );
-
-        let built = query_builder.build();
-        built.execute(&mut *txn).await?;
-    }
-
-    // 2. Read data from csv report. Fill mobile_radio_tracker if and only if location from csv and in mobile_hotspot_infos table matches
-    let mobile_infos_map: HashMap<_, _> = mobile_infos
-        .iter()
-        .map(|v| (bs58::encode(v.entity_key.clone()).into_string(), v.location))
-        .collect();
-    tracing::info!("Exporting data from CSV");
-    let mut rdr = Reader::from_path(csv_file_path)?;
-
-    #[derive(Debug, serde::Deserialize)]
-    struct Record {
-        public_key: PublicKeyBinary,
-        h3: String,
-        time: DateTime<Utc>,
-    }
-
-    let mut mobile_infos_to_update_map: HashMap<EntityKey, DateTime<Utc>> = HashMap::new();
-
-    let mut csv_migrated_counter = 0;
-    for record in rdr.deserialize() {
-        let record: Record = record?;
-        let pub_key: &str = &record.public_key.to_string();
-
-        if let Some(v) = mobile_infos_map.get(pub_key) {
-            let loc = i64::from_str_radix(&record.h3, 16).unwrap();
-            if v.unwrap() == loc {
-                let date_time = record.time;
-                let entity_key = bs58::decode(pub_key).into_vec()?;
-
-                mobile_infos_to_update_map.insert(entity_key, date_time);
-                csv_migrated_counter += 1;
-            }
-        } else {
-            tracing::warn!(
-                "Pubkey: {} exist in csv but not found in metadata database",
-                pub_key
+    for (k, _v) in tracked_radios {
+        if let Some(mobile_info) = mobile_infos.iter().find(|v| v.entity_key == k) {
+            sqlx::query(
+                r#"
+           UPDATE 
+"mobile_radio_tracker" SET asserted_location_changed_at = $1 WHERE entity_key = $2
+    "#,
             )
+            .bind(mobile_info.created_at)
+            .bind(&mobile_info.entity_key)
+            .execute(&mut *txn)
+            .await
+            .unwrap();
+        } else {
+            tracing::error!("Radio {} is not found", PublicKeyBinary::from(k))
         }
-    }
-    tracing::info!("Count radios migrated from CSV: {csv_migrated_counter}");
-
-    // 3. Set `asserted_location_changed_at = created_at` for others (num_location_asserts > 0)
-    for mi in mobile_infos.into_iter() {
-        mobile_infos_to_update_map
-            .entry(mi.entity_key)
-            .or_insert(mi.created_at);
-    }
-
-    let mobile_infos_to_update: Vec<(EntityKey, DateTime<Utc>)> =
-        mobile_infos_to_update_map.into_iter().collect();
-
-    tracing::info!("Updating asserted_location_changed_at in db");
-    for chunk in mobile_infos_to_update.chunks(BATCH_SIZE) {
-        let mut query_builder = QueryBuilder::new(
-            "UPDATE mobile_radio_tracker AS mrt SET asserted_location_changed_at = data.asserted_location_changed_at
-         FROM ( ",
-        );
-
-        query_builder.push_values(chunk, |mut builder, mob_info| {
-            builder.push_bind(&mob_info.0).push_bind(mob_info.1);
-        });
-
-        query_builder.push(
-            ") AS data(entity_key, asserted_location_changed_at)
-         WHERE mrt.entity_key = data.entity_key",
-        );
-
-        let built = query_builder.build();
-        built.execute(&mut *txn).await?;
     }
 
     txn.commit().await?;
+
+    let mut tracked_radios = get_tracked_radios(&mobile_config_pool).await?;
+    tracked_radios
+        .retain(|_k, v| v.asserted_location.is_some() && v.asserted_location_changed_at.is_none());
+    tracing::info!(
+        "Count of tracked radios with inconsistent info after migration: {}",
+        tracked_radios.len()
+    );
 
     Ok(())
 }
