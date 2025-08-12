0dc22c212edc28d9dfd254972935974b28f4a069
Michael Jeffrey <michaeldjeffrey@gmail.com>
2025-08-12 14:03:12 -0600

    Add metric when we throttle route stream updates (#1026)
    Broadcast channels will drop messages if one of the consumers falls 
    behind. We cannot miss updates for downstream HPR. This is why we check 
    how full the broadcast channel is before queueing a message. 
    
    Simply sleeping before adding a message means it’s theoretically 
    possible for messages to be queued out of order. 
    
    Before we spend resources trying to tackle this potential case, we 
    should add a way to check how common it might be.

--- iot_config/src/lib.rs
+++ iot_config/src/lib.rs
@@ -47,6 +47,7 @@ pub async fn broadcast_update<T: std::fmt::Debug>(
     sender: broadcast::Sender<T>,
 ) -> Result<(), broadcast::error::SendError<T>> {
     while !enqueue_update(sender.len()) {
+        telemetry::route_stream_throttle();
         tokio::time::sleep(tokio::time::Duration::from_millis(25)).await
     }
     sender.send(message).map(|_| ()).map_err(|err| {
