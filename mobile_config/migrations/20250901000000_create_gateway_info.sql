e85766472792a4b7b854eccfa02e19e888f2ee0a
Macpie <pierre@nova-labs.com>
2025-09-02 15:01:14 -0700

    Flatten table

--- mobile_config/migrations/20250901000000_create_gateway_info.sql
+++ mobile_config/migrations/20250901000000_create_gateway_info.sql
@@ -5,25 +5,17 @@ CREATE TYPE device_type AS ENUM (
     'wifiDataOnly'
 );
 
-CREATE TYPE deployment_info_v1 AS (
-    antena BIGINT,
-    elevation BIGINT,
-    azimuth BIGINT,
-    radio_id TEXT
-);
-
-CREATE TYPE location_info_v1 AS (
-    location BIGINT,
-    location_changed_at TIMESTAMPTZ,
-    location_asserts BIGINT
-);
-
 CREATE TABLE gateway_info (
     address BYTEA NOT NULL,
     device_type device_type NOT NULL,
-    location_info location_info_v1,
-    deployment_info deployment_info_v1,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    refreshed_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    refreshed_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    antena BIGINT,
+    elevation BIGINT,
+    azimuth BIGINT,
+    radio_id TEXT,
+    location BIGINT,
+    location_changed_at TIMESTAMPTZ,
+    location_asserts BIGINT
 );
