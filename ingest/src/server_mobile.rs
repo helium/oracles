647e490e10238d2f001439cee266c06282552d4e
Brian Balser <bbalser@nova-labs.com>
2025-09-16 07:40:17 -0400

    Update ingest

--- ingest/src/server_mobile.rs
+++ ingest/src/server_mobile.rs
@@ -619,9 +619,9 @@ fn is_data_transfer_for_cbrs(event: &DataTransferSessionReqV1) -> bool {
 }
 
 pub async fn grpc_server(settings: &Settings) -> Result<()> {
-    // Initialize uploader
+    let s3_client = settings.file_store.connect().await;
     let (file_upload, file_upload_server) =
-        file_upload::FileUpload::from_settings_tm(&settings.output).await?;
+        file_upload::FileUpload::new(s3_client, settings.output_bucket.clone()).await;
 
     let (wifi_heartbeat_report_sink, wifi_heartbeat_report_sink_server) =
         WifiHeartbeatIngestReportV1::file_sink(
