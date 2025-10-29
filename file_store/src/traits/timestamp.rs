a72c05228f2efb68cc0f5a28b11bcfd507c0ff35
Michael Jeffrey <michaeldjeffrey@gmail.com>
2025-10-29 13:40:27 -0700

    Add specific Error for handling timestamps
    `TimestampDecodeError` provides more explicit errors for what type of 
    field we are attempting to decode, and an extra if we cannot parse the 
    incoming number.
    
    `TimestampDecodeResult` is provided as a convenience for implementing 
    MsgTimestamp.
    
    ```
    impl MsgTimeStamp<Result<DateTime<Utc>, TimestampDecodeError>> for Type {}
    ```
    is super cumbersome to read.

--- file_store/src/traits/timestamp.rs
+++ file_store/src/traits/timestamp.rs
@@ -1,29 +1,71 @@
-use crate::{error::DecodeError, Result};
 use chrono::{DateTime, TimeZone, Utc};
 
+// This trait is not being provided by file-store proper
+// because it increaseses the chance of running into orphan rules.
+//
+// A common use is implementing MsgTimestamp for both the prost struct and
+// domain struct to ensure the same field is being referenced with the same
+// time unit. This becomes not possible if file-store provides the trait. The
+// implementer wouldn't own the trait and a high potential they wouldn't own
+// the proto struct definition and would be unable to provide half of the
+// benefit this trait provides.
+//
+// If you really want it, here's the trait in it's entirety with some examples,
+// put it in your own project.
+//
+// pub trait MsgTimestamp<R> {
+//     fn timestamp(&self) -> R;
+// }
+
+// impl MsgTimestamp<TimestampDecodeResult> for ProtoStruct {
+//     fn timestamp(&self) -> TimestampDecodeResult {
+//         self.received_timestamp.encode_timestamp_millis()
+//     }
+// }
+
+// impl MsgTimestamp<u64> for DomainStruct {
+//     fn timestamp(&self) -> u64 {
+//         self.received_timestamp.to_timestamp_millis()
+//     }
+// }
+
+pub type TimestampDecodeResult = Result<DateTime<Utc>, TimestampDecodeError>;
+
+#[derive(thiserror::Error, Debug)]
+pub enum TimestampDecodeError {
+    #[error("integer conversion error")]
+    FromInt(#[from] std::num::TryFromIntError),
+
+    #[error("invalid seconds: {0}")]
+    InvalidSeconds(u64),
+
+    #[error("invalid millis: {0}")]
+    InvalidMillis(u64),
+}
+
 pub trait TimestampDecode {
-    fn to_timestamp(self) -> Result<DateTime<Utc>>;
-    fn to_timestamp_millis(self) -> Result<DateTime<Utc>>;
-    fn to_timestamp_nanos(self) -> Result<DateTime<Utc>>;
+    fn to_timestamp(self) -> TimestampDecodeResult;
+    fn to_timestamp_millis(self) -> TimestampDecodeResult;
+    fn to_timestamp_nanos(self) -> TimestampDecodeResult;
 }
 
 impl TimestampDecode for u64 {
-    fn to_timestamp(self) -> Result<DateTime<Utc>> {
-        let decoded = i64::try_from(self).map_err(DecodeError::from)?;
+    fn to_timestamp(self) -> TimestampDecodeResult {
+        let decoded = i64::try_from(self)?;
         Utc.timestamp_opt(decoded, 0)
             .single()
-            .ok_or_else(|| DecodeError::invalid_timestamp(self))
+            .ok_or(TimestampDecodeError::InvalidSeconds(self))
     }
 
-    fn to_timestamp_millis(self) -> Result<DateTime<Utc>> {
-        let decoded = i64::try_from(self).map_err(DecodeError::from)?;
+    fn to_timestamp_millis(self) -> TimestampDecodeResult {
+        let decoded = i64::try_from(self)?;
         Utc.timestamp_millis_opt(decoded)
             .single()
-            .ok_or_else(|| DecodeError::invalid_timestamp(self))
+            .ok_or(TimestampDecodeError::InvalidMillis(self))
     }
 
-    fn to_timestamp_nanos(self) -> Result<DateTime<Utc>> {
-        let decoded = i64::try_from(self).map_err(DecodeError::from)?;
+    fn to_timestamp_nanos(self) -> TimestampDecodeResult {
+        let decoded = i64::try_from(self)?;
         Ok(Utc.timestamp_nanos(decoded))
     }
 }
