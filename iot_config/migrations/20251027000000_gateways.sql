c6a63bfb3bfd2707f7c1aca1588b149f82273bcf
Macpie <pierre@nova-labs.com>
2025-10-29 11:04:55 -0700

    Connect old APIs to new DB

--- iot_config/migrations/20251027000000_gateways.sql
+++ iot_config/migrations/20251027000000_gateways.sql
@@ -13,3 +13,9 @@ CREATE TABLE IF NOT EXISTS gateways (
     refreshed_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS gateways_last_changed_idx ON gateways (last_changed_at DESC);
+
+CREATE INDEX IF NOT EXISTS gateways_location_changed_idx ON gateways (location_changed_at DESC)
+WHERE
+    location IS NOT NULL;
