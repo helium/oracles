88e9d3d67bad6c0f0670a606618dcae5212803bd
Michael Jeffrey <michaeldjeffrey@gmail.com>
2025-12-12 14:53:54 -0700

    Provide AWS Error mapping per operation we use (#1104)
    Using `aws_sdk_s3::Error` directly has the chance that we throw away
    some useful information in the error conversion. Breaking the errors
    into Operations (simillar to how the sdk handles errors internally) we
    can retain some of that top level information.
    
    Internal AWS Errors will print certain errors, if there’s a proided
    code. Otherwise they say “unhandled” which isn’t always useful.
    https://github.com/awslabs/aws-sdk-rust/blob/3611b7c8306750803fc75757b97092ff6d8ba6fa/sdk/s3/src/error_meta.rs#L72-L77
    
    An example from another Projects CI where we have very little
    information. We had no clue as to what operation was causing this error.
    ```
    thread 'etl_v2::mobile_hotspot_usage::simple_2_carriers_multiple_pubkeys' panicked at tests/integrations/etl_v2/runner.rs:80:25:
    ETL failed: aws error: s3: unhandled error
    
    Caused by:
        s3: unhandled error
    ```
    
    Now, at least we’ll know which operation we can start interograting.
    ```
    Error: aws error: put_object: unhandled error
    
    Caused by:
        put_object: unhandled error
    ```

--- file_store/src/error.rs
+++ file_store/src/error.rs
@@ -4,6 +4,8 @@ use thiserror::Error;
 
 use crate::{file_info::FileInfoError, gzipped_framed_file::GzippedFramedFileError};
 
+pub use aws_error::AwsError;
+
 pub type Result<T = ()> = std::result::Result<T, Error>;
 
 #[derive(Error, Debug)]
@@ -37,24 +39,6 @@ pub enum Error {
     // Not recommended for internal use!
     #[error("external error")]
     ExternalError(#[from] Box<dyn std::error::Error + Send + Sync>),
-}
-
-#[derive(thiserror::Error, Debug)]
-pub enum AwsError {
-    #[error("s3: {0}")]
-    S3(Box<aws_sdk_s3::Error>),
-
-    #[error("streaming: {0}")]
-    Streaming(#[from] aws_sdk_s3::primitives::ByteStreamError),
-}
-
-impl AwsError {
-    pub fn s3_error<T>(err: T) -> Error
-    where
-        T: Into<aws_sdk_s3::Error>,
-    {
-        Error::from(err.into())
-    }
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -99,14 +83,55 @@ impl ChannelError {
     }
 }
 
-impl From<aws_sdk_s3::Error> for Error {
-    fn from(err: aws_sdk_s3::Error) -> Self {
-        Self::Aws(AwsError::S3(Box::new(err)))
-    }
-}
+mod aws_error {
+    use super::Error;
 
-impl From<aws_sdk_s3::primitives::ByteStreamError> for Error {
-    fn from(err: aws_sdk_s3::primitives::ByteStreamError) -> Self {
-        Self::from(AwsError::Streaming(err))
+    use aws_sdk_s3::error::SdkError;
+    use aws_sdk_s3::{
+        operation::{
+            delete_object::DeleteObjectError, get_object::GetObjectError,
+            list_objects_v2::ListObjectsV2Error, put_object::PutObjectError,
+        },
+        primitives::ByteStreamError,
+    };
+
+    #[derive(thiserror::Error, Debug)]
+    pub enum AwsError {
+        #[error("put_object: {0}")]
+        PutObject(PutObjectError),
+
+        #[error("pub_object_byte_stream: {0}")]
+        PubObjectByteStream(ByteStreamError),
+
+        #[error("delete_object: {0}")]
+        DeleteObject(DeleteObjectError),
+
+        #[error("get_object: {0}")]
+        GetObject(GetObjectError),
+
+        #[error("list_object: {0}")]
+        ListObject(ListObjectsV2Error),
+    }
+
+    impl AwsError {
+        pub fn put_object_error(err: SdkError<PutObjectError>) -> Error {
+            Error::Aws(Self::PutObject(err.into_service_error()))
+        }
+
+        pub fn pub_object_byte_stream_error(err: ByteStreamError) -> Error {
+            Error::Aws(Self::PubObjectByteStream(err))
+        }
+
+        pub fn delete_object_error(err: SdkError<DeleteObjectError>) -> Error {
+            Error::Aws(Self::DeleteObject(err.into_service_error()))
+        }
+
+        pub fn get_object_error(err: SdkError<GetObjectError>) -> Error {
+            Error::Aws(Self::GetObject(err.into_service_error()))
+        }
+
+        pub fn list_object_error(err: SdkError<ListObjectsV2Error>) -> Error {
+            Error::Aws(Self::ListObject(err.into_service_error()))
+        }
     }
 }
