3aa347f29fe527a50cef6b0ce88ee69010790a0f
Anatolii Kurotych <akurotych@gmail.com>
2026-01-30 10:37:40 +0200

    add gateway_stream_info_v4_min_owner_changed_at test

--- mobile_config/tests/integrations/gateway_service_v4.rs
+++ mobile_config/tests/integrations/gateway_service_v4.rs
@@ -118,6 +118,81 @@ async fn gateway_stream_info_v4_filters_null_owner(pool: PgPool) -> anyhow::Resu
     Ok(())
 }
 
+#[sqlx::test]
+async fn gateway_stream_info_v4_min_owner_changed_at(pool: PgPool) -> anyhow::Result<()> {
+    let admin_key = make_keypair();
+
+    let address1 = make_keypair().public_key().clone();
+    let address2 = make_keypair().public_key().clone();
+
+    let now = Utc::now();
+    let now_minus_six = now - Duration::hours(6);
+    let now_minus_four = now - Duration::hours(4);
+    let now_minus_three = now - Duration::hours(3);
+    let now_minus_five = now - Duration::hours(5);
+
+    // Gateway1: owner changed at now - 6 hours (older)
+    let gateway1: Gateway = TestGatewayBuilder::default()
+        .address(address1.clone())
+        .gateway_type(GatewayType::WifiIndoor)
+        .created_at(now_minus_six)
+        .inserted_at(now_minus_six)
+        .refreshed_at(now)
+        .last_changed_at(now_minus_three)
+        .owner(Some("owner1".to_string()))
+        .owner_changed_at(Some(now_minus_six))
+        .build()?
+        .into();
+    gateway1.insert(&pool).await?;
+
+    // Gateway2: owner changed at now - 4 hours (newer)
+    let gateway2: Gateway = TestGatewayBuilder::default()
+        .address(address2.clone())
+        .gateway_type(GatewayType::WifiOutdoor)
+        .created_at(now_minus_six)
+        .inserted_at(now_minus_six)
+        .refreshed_at(now)
+        .last_changed_at(now_minus_three)
+        .owner(Some("owner2".to_string()))
+        .owner_changed_at(Some(now_minus_four))
+        .build()?
+        .into();
+    gateway2.insert(&pool).await?;
+
+    let (addr, _handle) = spawn_gateway_service(pool.clone(), admin_key.public_key().clone()).await;
+    let mut client = GatewayClient::connect(addr).await?;
+
+    // min_owner_changed_at = now - 5 hours. Only gateway2 (changed at now - 4h) should pass
+    let resp = gateway_info_stream_v4(
+        &mut client,
+        &admin_key,
+        &[],
+        0,
+        0,
+        now_minus_five.timestamp() as u64,
+    )
+    .await?;
+    assert_eq!(resp.gateways.len(), 1);
+
+    let gateway: &GatewayInfoV4 = resp.gateways.first().unwrap();
+    assert_eq!(gateway.address, address2.to_vec());
+    assert_eq!(gateway.owner, "owner2");
+
+    // min_owner_changed_at = now - 6 hours both should pass
+    let resp = gateway_info_stream_v4(
+        &mut client,
+        &admin_key,
+        &[],
+        0,
+        0,
+        now_minus_six.timestamp() as u64,
+    )
+    .await?;
+    assert_eq!(resp.gateways.len(), 2);
+
+    Ok(())
+}
+
 async fn gateway_info_stream_v4(
     client: &mut GatewayClient<tonic::transport::Channel>,
     signer: &Keypair,
